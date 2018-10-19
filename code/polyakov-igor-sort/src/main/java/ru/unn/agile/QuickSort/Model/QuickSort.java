ae06349b3736c475709433d745c55a2bbbc14d0f
Polyakov Igor <polykovio@mail.ru>
2018-10-20 00:08:58 +0300

    Refactoring. Added swap method

--- code/polyakov-igor-sort/src/main/java/ru/unn/agile/QuickSort/Model/QuickSort.java
+++ code/polyakov-igor-sort/src/main/java/ru/unn/agile/QuickSort/Model/QuickSort.java
@@ -4,10 +4,14 @@ public class QuickSort {
     public static void quickSort(int[] array) {
         if (array.length == 2) {
             if (array[0] > array[1]) {
-                int tmp = array[0];
-                array[0] = array[1];
-                array[1] = tmp;
+                swap(array, 0, 1);
             }
         }
+    }
+
+    private static void swap(int[] array, int pos_1, int pos_2) {
+        int tmp = array[pos_1];
+        array[pos_1] = array[pos_2];
+        array[pos_2] = tmp;
     }
 }
