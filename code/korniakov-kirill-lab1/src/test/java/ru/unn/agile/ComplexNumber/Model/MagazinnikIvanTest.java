a3171f5011ab99d95bdb5a7b685836477ece4d3f
Ivan Magazinnik <magazinnik.ivan@gmail.com>
2018-10-20 11:48:43 +0300

    Add tests with Double.MAX_VALUE

---
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/MagazinnikIvanTest.java
@@ -0,0 +1,34 @@
+package ru.unn.agile.complexnumber.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MagazinnikIvanTest {
+    @Test
+    public void canGetMaxDoubleImPart() {
+        ComplexNumber z = new ComplexNumber(0, Double.MAX_VALUE);
+
+        double imaginary = z.getImaginary();
+
+        assertEquals(imaginary, Double.MAX_VALUE, 0.000001);
+    }
+
+    @Test
+    public void canGetMaxDoubleRealPart() {
+        ComplexNumber z = new ComplexNumber(Double.MAX_VALUE, 0);
+
+        double real = z.getReal();
+
+        assertEquals(real, Double.MAX_VALUE, 0.000001);
+    }
+
+    @Test
+    public void canAddNumbersWithMaxRealAndImageParts() {
+        ComplexNumber z1 = new ComplexNumber(Double.MAX_VALUE, 0);
+        ComplexNumber z2 = new ComplexNumber(0, Double.MAX_VALUE);
+
+        ComplexNumber z = z1.add(z2);
+
+        assertEquals(new ComplexNumber(Double.MAX_VALUE, Double.MAX_VALUE), z);
+    }
+}
