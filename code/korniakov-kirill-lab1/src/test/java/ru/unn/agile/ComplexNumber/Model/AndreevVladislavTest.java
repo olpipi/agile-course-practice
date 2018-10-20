88a9e5c62ca210c85c02218347f8b6d8fe01b552
inhabitantNewCity <v.andreev.job@gmail.com>
2018-10-20 16:05:43 +0300

    remove redundant empty line

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/AndreevVladislavTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/AndreevVladislavTest.java
@@ -10,46 +10,36 @@ import static org.junit.Assert.*;
 public class AndreevVladislavTest {
     @Test
     public void canAddComplexNumbers() {
-
         ComplexNumber z1 = new ComplexNumber(1, 0);
         ComplexNumber z2 = new ComplexNumber(0, 1);
 
-
         ComplexNumber z = z1.add(z2);
-
 
         assertEquals(new ComplexNumber(1, 1), z);
     }
 
     @Test
     public void canAddNegativeNumbers() {
-
         ComplexNumber z1 = new ComplexNumber(-1, 0);
         ComplexNumber z2 = new ComplexNumber(-2, 1);
 
-
         ComplexNumber z = z1.add(z2);
-
 
         assertEquals(new ComplexNumber(-3, 1), z);
     }
 
     @Test
     public void canAddZeroNumbers() {
-
         ComplexNumber z1 = new ComplexNumber(0, 0);
         ComplexNumber z2 = new ComplexNumber(0, 0);
 
-
         ComplexNumber z = z1.add(z2);
-
 
         assertEquals(new ComplexNumber(0, 0), z);
     }
 
     @Test
     public void canToString() {
-
         ComplexNumber z1 = new ComplexNumber(-3.2, 5.7);
 
         String z1String = z1.toString();
