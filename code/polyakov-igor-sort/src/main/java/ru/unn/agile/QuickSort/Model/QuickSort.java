0b1b7dfdac9d38ff5fa888e6d436e0b927cb88d5
Polyakov Igor <polykovio@mail.ru>
2018-10-20 09:34:10 +0300

    Refactoring

--- code/polyakov-igor-sort/src/main/java/ru/unn/agile/QuickSort/Model/QuickSort.java
+++ code/polyakov-igor-sort/src/main/java/ru/unn/agile/QuickSort/Model/QuickSort.java
@@ -1,17 +1,21 @@
 package ru.unn.agile.QuickSort.Model;
 
 public class QuickSort {
-    public static void quickSort(int[] array) {
+    public static void quickSort(final int[] array) {
         if (array.length == 2) {
             if (array[0] > array[1]) {
-                swap(array, 0, 1);
+                swapTwoElementsInArray(array, 0, 1);
             }
         }
     }
 
-    private static void swap(int[] array, int pos_1, int pos_2) {
-        int tmp = array[pos_1];
-        array[pos_1] = array[pos_2];
-        array[pos_2] = tmp;
+    private static void swapTwoElementsInArray(final int[] array, int pos_element_1, int pos_element_2) {
+        int tmp = array[pos_element_1];
+        array[pos_element_1] = array[pos_element_2];
+        array[pos_element_2] = tmp;
+    }
+
+    private QuickSort() {
+
     }
 }
