169c3c8bcb605608e4cfc3d534cc277c71824dd0
Polyakov Igor <polykovio@mail.ru>
2018-10-20 14:39:55 +0300

    Added simple implementation of quick sort for integers

--- code/polyakov-igor-sort/src/main/java/ru/unn/agile/QuickSort/Model/QuickSort.java
+++ code/polyakov-igor-sort/src/main/java/ru/unn/agile/QuickSort/Model/QuickSort.java
@@ -1,18 +1,40 @@
 package ru.unn.agile.QuickSort.Model;
 
 public class QuickSort {
-    public static void quickSort(final int[] array) {
-        if (array.length == 2) {
-            if (array[0] > array[1]) {
-                swapTwoElementsInArray(array, 0, 1);
+    public static void quickSort(final int[] array, int startElementIndex, int endElementIndex) {
+        int i = startElementIndex;
+        int j = endElementIndex;
+        int bearingElement = array[startElementIndex];
+        while (i <= j) {
+            while (array[i] < bearingElement) {
+                ++i;
             }
+
+            while (array[j] > bearingElement) {
+                --j;
+            }
+
+            if (i <= j) {
+                swapTwoElementsInArray(array, i, j);
+                ++i;
+                --j;
+            }
+        }
+
+        if (j > startElementIndex) {
+            quickSort(array, startElementIndex, j);
+        }
+        if (i < endElementIndex) {
+            quickSort(array, i, endElementIndex);
         }
     }
 
-    private static void swapTwoElementsInArray(final int[] array, final int firstElement, final int secondElement) {
-        int tmp = array[firstElement];
-        array[firstElement] = array[secondElement];
-        array[secondElement] = tmp;
+    private static void swapTwoElementsInArray(final int[] array,
+                                               final int firstElementIndex,
+                                               final int secondElementIndex) {
+        int tmp = array[firstElementIndex];
+        array[firstElementIndex] = array[secondElementIndex];
+        array[secondElementIndex] = tmp;
     }
 
     private QuickSort() {
--- code/polyakov-igor-sort/src/test/java/ru/unn/agile/QuickSort/Model/QuickSortTest.java
+++ code/polyakov-igor-sort/src/test/java/ru/unn/agile/QuickSort/Model/QuickSortTest.java
@@ -18,7 +18,7 @@ public class QuickSortTest {
     public void canSortOneInteger() {
         int[] array = {1};
 
-        QuickSort.quickSort(array);
+        QuickSort.quickSort(array, 0, array.length - 1);
 
         assertTrue(isArraySorted(array));
     }
@@ -27,7 +27,7 @@ public class QuickSortTest {
     public void canSortTwoSortedIntegers() {
         int[] array = {1, 2};
 
-        QuickSort.quickSort(array);
+        QuickSort.quickSort(array, 0, array.length - 1);
 
         assertTrue(isArraySorted(array));
     }
@@ -36,7 +36,16 @@ public class QuickSortTest {
     public void canSortTwoNonSortedIntegers() {
         int[] array = {2, 1};
 
-        QuickSort.quickSort(array);
+        QuickSort.quickSort(array, 0, array.length - 1);
+
+        assertTrue(isArraySorted(array));
+    }
+
+    @Test
+    public void canSortThreeNonSortedIntegers() {
+        int[] array = {2, 0, 1};
+
+        QuickSort.quickSort(array, 0, array.length - 1);
 
         assertTrue(isArraySorted(array));
     }
