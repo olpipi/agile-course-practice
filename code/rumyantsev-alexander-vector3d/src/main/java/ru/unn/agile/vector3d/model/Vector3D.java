86f465d50cb677efa7995c9dd8f141d9ef8a75d6
Alexander Rumyantsev <cetygamer@gmail.com>
2018-10-21 12:14:51 +0300

    initial structure commit

---
+++ code/rumyantsev-alexander-vector3d/src/main/java/ru/unn/agile/vector3d/model/Vector3D.java
@@ -0,0 +1,4 @@
+package ru.unn.agile.vector3d.model;
+
+public class Vector3D {
+}
---
+++ code/rumyantsev-alexander-vector3d/src/test/java/ru/unn/agile/vector3d/model/Vector3DTest.java
@@ -0,0 +1,11 @@
+package ru.unn.agile.vector3d.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Vector3DTest {
+    @Test
+    public void dummy() {
+        assert true;
+    }
+}
