010777a765457583e3f3f954f56d969c2c6969ad
Polyakov Igor <polykovio@mail.ru>
2018-10-21 14:53:05 +0300

    Redesigning of QuickSort class

--- code/polyakov-igor-sort/src/main/java/ru/unn/agile/QuickSort/Model/QuickSort.java
+++ code/polyakov-igor-sort/src/main/java/ru/unn/agile/QuickSort/Model/QuickSort.java
@@ -1,81 +1,85 @@
 package ru.unn.agile.QuickSort.Model;
 
 public final class QuickSort {
-    public static final int MIN_ARRAY_ELEMENTS_COUNT = 1;
-
     public static <T extends Comparable<T>>
     void sort(final T[] array) {
-        validateArray(array);
-
-        int startElementIndex = 0;
-        int endElementIndex = array.length - 1;
-        sortSubArray(array, startElementIndex, endElementIndex);
-    }
-
-    private static <T extends Comparable<T>>
-    void validateArray(final T[] array) {
-        if (array == null) {
-            throw new IllegalArgumentException("Array should be initialized");
-        }
-
-        if (array.length < MIN_ARRAY_ELEMENTS_COUNT) {
-            throw new IllegalArgumentException("Array elements count should be positive");
-        }
-
-        for (int i = 0; i < array.length; ++i) {
-            if (array[i] == null)
-                throw new IllegalArgumentException("Array shouldn't contain null elements");
-        }
-    }
-
-    private static <T extends Comparable<T>>
-    void sortSubArray(final T[] array,
-                      final int startSubArrayIndex,
-                      final int endSubArrayIndex) {
-        T bearingElement = calculateBearingElement(array, startSubArrayIndex, endSubArrayIndex);
-
-        int currentLeftBound = startSubArrayIndex;
-        int currentRightBound = endSubArrayIndex;
-        while (currentLeftBound <= currentRightBound) {
-            while (array[currentLeftBound].compareTo(bearingElement) < 0) {
-                ++currentLeftBound;
-            }
-
-            while (array[currentRightBound].compareTo(bearingElement) > 0) {
-                --currentRightBound;
-            }
-
-            if (currentLeftBound <= currentRightBound) {
-                swapTwoElementsInArray(array, currentLeftBound, currentRightBound);
-                ++currentLeftBound;
-                --currentRightBound;
-            }
-        }
-
-        if (currentRightBound > startSubArrayIndex) {
-            sortSubArray(array, startSubArrayIndex, currentRightBound);
-        }
-        if (currentLeftBound < endSubArrayIndex) {
-            sortSubArray(array, currentLeftBound, endSubArrayIndex);
-        }
-    }
-
-    private static <T extends Comparable<T>>
-    T calculateBearingElement(final T[] array,
-                              final int firstElementIndex,
-                              final int secondElementIndex) {
-        return array[(firstElementIndex + secondElementIndex) / 2];
-    }
-
-    private static <T extends Comparable<T>>
-    void swapTwoElementsInArray(final T[] array,
-                                final int firstElementIndex,
-                                final int secondElementIndex) {
-        T tmp = array[firstElementIndex];
-        array[firstElementIndex] = array[secondElementIndex];
-        array[secondElementIndex] = tmp;
+        QuickSortImpl<T> sorter = new QuickSortImpl<T>();
+        sorter.sort(array);
     }
 
     private QuickSort() {
+    }
+
+    private static final class QuickSortImpl<T extends Comparable<T>> {
+        public static final int MIN_ARRAY_ELEMENTS_COUNT = 1;
+
+        void sort(final T[] array) {
+            validateArray(array);
+
+            int startElementIndex = 0;
+            int endElementIndex = array.length - 1;
+            sortSubArray(array, startElementIndex, endElementIndex);
+        }
+
+        private void validateArray(final T[] array) {
+            if (array == null) {
+                throw new IllegalArgumentException("Array should be initialized");
+            }
+
+            if (array.length < MIN_ARRAY_ELEMENTS_COUNT) {
+                throw new IllegalArgumentException("Array elements count should be positive");
+            }
+
+            for (int i = 0; i < array.length; ++i) {
+                if (array[i] == null) {
+                    throw new IllegalArgumentException("Array shouldn't contain null elements");
+                }
+            }
+        }
+
+        private void sortSubArray(final T[] array,
+                                  final int startSubArrayIndex,
+                                  final int endSubArrayIndex) {
+            T bearingElement = calculateBearingElement(array, startSubArrayIndex, endSubArrayIndex);
+
+            int currentLeftBound = startSubArrayIndex;
+            int currentRightBound = endSubArrayIndex;
+            while (currentLeftBound <= currentRightBound) {
+                while (array[currentLeftBound].compareTo(bearingElement) < 0) {
+                    ++currentLeftBound;
+                }
+
+                while (array[currentRightBound].compareTo(bearingElement) > 0) {
+                    --currentRightBound;
+                }
+
+                if (currentLeftBound <= currentRightBound) {
+                    swapTwoElementsInArray(array, currentLeftBound, currentRightBound);
+                    ++currentLeftBound;
+                    --currentRightBound;
+                }
+            }
+
+            if (currentRightBound > startSubArrayIndex) {
+                sortSubArray(array, startSubArrayIndex, currentRightBound);
+            }
+            if (currentLeftBound < endSubArrayIndex) {
+                sortSubArray(array, currentLeftBound, endSubArrayIndex);
+            }
+        }
+
+        private T calculateBearingElement(final T[] array,
+                                          final int firstElementIndex,
+                                          final int secondElementIndex) {
+            return array[(firstElementIndex + secondElementIndex) / 2];
+        }
+
+        private void swapTwoElementsInArray(final T[] array,
+                                            final int firstElementIndex,
+                                            final int secondElementIndex) {
+            T tmp = array[firstElementIndex];
+            array[firstElementIndex] = array[secondElementIndex];
+            array[secondElementIndex] = tmp;
+        }
     }
 }
