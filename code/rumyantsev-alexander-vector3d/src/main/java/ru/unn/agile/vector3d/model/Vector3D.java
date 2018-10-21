3dea6cf1a6eeba3979c7332462f2321e593ce3c1
Alexander Rumyantsev <cetygamer@gmail.com>
2018-10-21 14:09:05 +0300

    added add, subtract, multiply + tests

--- code/rumyantsev-alexander-vector3d/src/main/java/ru/unn/agile/vector3d/model/Vector3D.java
+++ code/rumyantsev-alexander-vector3d/src/main/java/ru/unn/agile/vector3d/model/Vector3D.java
@@ -53,4 +53,16 @@ public class Vector3D {
 
         return false;
     }
+
+    public Vector3D add(Vector3D other) {
+        return new Vector3D(this.x + other.x, this.y + other.y, this.z + other.z);
+    }
+
+    public Vector3D subtract(Vector3D other) {
+        return new Vector3D(this.x - other.x, this.y - other.y, this.z - other.z);
+    }
+
+    public Vector3D multiply(double coef) {
+        return new Vector3D(this.x * coef, this.y * coef, this.z * coef);
+    }
 }
--- code/rumyantsev-alexander-vector3d/src/test/java/ru/unn/agile/vector3d/model/Vector3DTest.java
+++ code/rumyantsev-alexander-vector3d/src/test/java/ru/unn/agile/vector3d/model/Vector3DTest.java
@@ -1,6 +1,7 @@
 package ru.unn.agile.vector3d.model;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class Vector3DTest {
@@ -48,7 +49,7 @@ public class Vector3DTest {
 
     @Test
     public void canSetXPositiveDouble() {
-        Vector3D v = new Vector3D(0,0, 0);
+        Vector3D v = new Vector3D(0, 0, 0);
 
         v.setX(0.5);
 
@@ -57,7 +58,7 @@ public class Vector3DTest {
 
     @Test
     public void canSetXNegativeDouble() {
-        Vector3D v = new Vector3D(0,0, 0);
+        Vector3D v = new Vector3D(0, 0, 0);
 
         v.setX(-3.3);
 
@@ -66,7 +67,7 @@ public class Vector3DTest {
 
     @Test
     public void canSetYPositiveDouble() {
-        Vector3D v = new Vector3D(0,0, 0);
+        Vector3D v = new Vector3D(0, 0, 0);
 
         v.setY(0.144);
 
@@ -75,7 +76,7 @@ public class Vector3DTest {
 
     @Test
     public void canSetYNegativeDouble() {
-        Vector3D v = new Vector3D(0,0, 0);
+        Vector3D v = new Vector3D(0, 0, 0);
 
         v.setY(-3.1415);
 
@@ -84,7 +85,7 @@ public class Vector3DTest {
 
     @Test
     public void canSetZPositiveDouble() {
-        Vector3D v = new Vector3D(0,0, 0);
+        Vector3D v = new Vector3D(0, 0, 0);
 
         v.setZ(43.5);
 
@@ -93,7 +94,7 @@ public class Vector3DTest {
 
     @Test
     public void canSetZNegativeDouble() {
-        Vector3D v = new Vector3D(0,0, 0);
+        Vector3D v = new Vector3D(0, 0, 0);
 
         v.setZ(-25.4);
 
@@ -165,8 +166,106 @@ public class Vector3DTest {
     @Test
     public void compareVectorsDifferentZLessEps() {
         Vector3D v1 = new Vector3D(0, 0, 1000000);
-        Vector3D v2 = new Vector3D(0, 0, 1000000 -Vector3D.EPSILON * 1.1);
+        Vector3D v2 = new Vector3D(0, 0, 1000000 - Vector3D.EPSILON * 1.1);
 
         assertNotEquals(v2, v1);
+    }
+
+    @Test
+    public void canAddVectors1() {
+        Vector3D v1 = new Vector3D(1, 2, 3);
+        Vector3D v2 = new Vector3D(3, 2, 1);
+        Vector3D expectedRes = new Vector3D(4, 4, 4);
+
+        Vector3D res = v1.add(v2);
+
+        assertEquals(expectedRes, res);
+    }
+
+    @Test
+    public void canAddVectors2() {
+        Vector3D v1 = new Vector3D(-1, -2, -3);
+        Vector3D v2 = new Vector3D(3, 2, 1);
+        Vector3D expectedRes = new Vector3D(2, 0, -2);
+
+        Vector3D res = v1.add(v2);
+
+        assertEquals(expectedRes, res);
+    }
+
+    @Test
+    public void canAddVectorsMultipleTimes() {
+        Vector3D v1 = new Vector3D(19.0, 12.0, -42.0);
+        Vector3D v2 = new Vector3D(-16.0, 16.0, -28.0);
+        Vector3D v3 = new Vector3D(16.0, -20.0, 46.0);
+        Vector3D expectedRes = new Vector3D(19.0, 8.0, -24.0);
+
+        Vector3D res = v1.add(v2).add(v3);
+
+        assertEquals(expectedRes, res);
+    }
+
+    @Test
+    public void canSubtractVectors1() {
+        Vector3D v1 = new Vector3D(10, 20, 30);
+        Vector3D v2 = new Vector3D(-5, 2, 30);
+        Vector3D expectedRes = new Vector3D(15, 18, 0);
+
+        Vector3D res = v1.subtract(v2);
+
+        assertEquals(expectedRes, res);
+    }
+
+    @Test
+    public void canSubtractVectors2() {
+        Vector3D v1 = new Vector3D(4, -5, 10);
+        Vector3D v2 = new Vector3D(14, 22, -40);
+        Vector3D expectedRes = new Vector3D(-10, -27, 50);
+
+        Vector3D res = v1.subtract(v2);
+
+        assertEquals(expectedRes, res);
+    }
+
+    @Test
+    public void canSubtractVectorsMultipleTimes() {
+        Vector3D v1 = new Vector3D(0.17, 0.1, -0.26);
+        Vector3D v2 = new Vector3D(0.16, 0.42, -0.28);
+        Vector3D v3 = new Vector3D(-0.29, 0.2, 0.48);
+        Vector3D expectedRes = new Vector3D(0.3, -0.52, -0.46);
+
+        Vector3D res = v1.subtract(v2).subtract(v3);
+
+        assertEquals(expectedRes, res);
+    }
+
+    @Test
+    public void canMultiplyByPositiveScalar() {
+        Vector3D v = new Vector3D(4, 5, 6);
+        Vector3D expectedRes = new Vector3D(40, 50, 60);
+
+        Vector3D res = v.multiply(10);
+
+        assertEquals(expectedRes, res);
+    }
+
+    @Test
+    public void canMultiplyByNegativeScalar() {
+        Vector3D v = new Vector3D(0, 7, -16);
+        Vector3D expectedRes = new Vector3D(0, -7, 16);
+
+        Vector3D res = v.multiply(-1);
+
+        assertEquals(expectedRes, res);
+    }
+
+    @Test
+    public void canMultiplyByZeroScalar() {
+        Vector3D v = new Vector3D(1674.3, 0, -5602.6);
+        Vector3D expectedRes = new Vector3D(0, 0, 0);
+
+        Vector3D res = v.multiply(0);
+
+        assertEquals(expectedRes, res);
     }
 }
