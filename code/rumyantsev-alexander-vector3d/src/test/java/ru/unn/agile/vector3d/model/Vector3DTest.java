b1e565f1e3a12ba5d7a0f8787f034165891a4629
Alexander Rumyantsev <cetygamer@gmail.com>
2018-10-21 12:36:09 +0300

    added another getX test

--- code/rumyantsev-alexander-vector3d/src/test/java/ru/unn/agile/vector3d/model/Vector3DTest.java
+++ code/rumyantsev-alexander-vector3d/src/test/java/ru/unn/agile/vector3d/model/Vector3DTest.java
@@ -5,9 +5,16 @@ import static org.junit.Assert.*;
 
 public class Vector3DTest {
     @Test
-    public void canGetX() {
-        Vector3D v = new Vector3D(1, 2, 3);
+    public void canGetXPositiveDouble() {
+        Vector3D v = new Vector3D(1.0, 2, 3);
 
-        assertEquals(1, v.getX(), Vector3D.EPSILON);
+        assertEquals(1.0, v.getX(), Vector3D.EPSILON);
+    }
+
+    @Test
+    public void canGetXNegativeDouble() {
+        Vector3D v = new Vector3D(-1.3, 0, 0);
+
+        assertEquals(-1.3, v.getX(), Vector3D.EPSILON);
     }
 }
