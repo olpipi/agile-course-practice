d9f9898f8b0f4400a173869a951b01de056943cb
alexanderflegontov <manshipnn@gmail.com>
2018-10-21 20:42:10 +0300

    Added more unit tests

---
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/FlegontovAlexanderTest.java
@@ -0,0 +1,29 @@
+package ru.unn.agile.complexnumber.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FlegontovAlexanderTest {
+    @Test
+    public void canAddNegativeComplexNumbers() {
+        ComplexNumber c1 = new ComplexNumber(-1, -2);
+        ComplexNumber c2 = new ComplexNumber(-3, -4);
+        ComplexNumber summa = c2.add(c1);
+        assertTrue(summa.equals(new ComplexNumber(-4, -6)));
+    }
+
+    @Test
+    public void canMultComplexNumbers() {
+        ComplexNumber c1 = new ComplexNumber(0.6, 0.33);
+        ComplexNumber c2 = new ComplexNumber(8, 1);
+        ComplexNumber mult = c2.multiply(c1);
+        assertEquals(new ComplexNumber(4.47, 3.24), mult);
+    }
+
+    @Test
+    public void areComplexNumbersEquivalent() {
+        ComplexNumber c1 = new ComplexNumber(1.1, 4.3);
+        ComplexNumber c2 = new ComplexNumber(1.1, 4.3);
+        assertTrue(c2.equals(c1));
+    }
+}
