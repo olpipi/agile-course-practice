c86d1a160cbbb36e2cea0736e848144f7a4ed556
metelev.evgenii <metelev.evgenii@yandex.ru>
2018-10-22 23:20:05 +0300

    Finish tests with fullCost

--- code/metelev-evgenii-mortgage-calculator/src/main/java/ru/unn/agile/MortgageCalculator/Model/MortgageCalculator.java
+++ code/metelev-evgenii-mortgage-calculator/src/main/java/ru/unn/agile/MortgageCalculator/Model/MortgageCalculator.java
@@ -2,6 +2,7 @@ package ru.unn.agile.MortgageCalculator.Model;
 
 public class MortgageCalculator {
     private String typeOfPayment;
+    private float fullCostOfApartment;
     public void setTypeOfPayment(String AnnuityOrDifferentiated) {
         switch (AnnuityOrDifferentiated){
             case "Annuity":
@@ -18,5 +19,16 @@ public class MortgageCalculator {
 
     public String getTypeOfPayment() {
         return typeOfPayment;
+    }
+
+    public void setFullCostOfApartment(float sum) {
+        if (sum>0)
+            fullCostOfApartment = sum;
+        else
+            fullCostOfApartment = -1;
+    }
+
+    public float getFullCostOfApartment() {
+        return fullCostOfApartment;
     }
 }
--- code/metelev-evgenii-mortgage-calculator/src/test/java/ru/unn/agile/MortgageCalculator/Model/MortgageCalculatorTest.java
+++ code/metelev-evgenii-mortgage-calculator/src/test/java/ru/unn/agile/MortgageCalculator/Model/MortgageCalculatorTest.java
@@ -4,36 +4,53 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 public class MortgageCalculatorTest {
 
+    final double epsilon = 0.000000001;
     @Test
-    public void geTypeOfPaymentIsAnnuity() {
+    public void checkTypeOfPaymentIsAnnuity() {
         MortgageCalculator myMortgageCalculator = new MortgageCalculator();
-
         myMortgageCalculator.setTypeOfPayment("Annuity");
-
         assertEquals("Annuity",myMortgageCalculator.getTypeOfPayment());
     }
     @Test
-    public void geTypeOfPaymentIsDifferentiated() {
+    public void checkTypeOfPaymentIsDifferentiated() {
         MortgageCalculator myMortgageCalculator = new MortgageCalculator();
-
         myMortgageCalculator.setTypeOfPayment("Differentiated");
-
         assertEquals("Differentiated",myMortgageCalculator.getTypeOfPayment());
     }
     @Test
-    public void geTypeOfPaymentIsNotCorrect() {
+    public void checkTypeOfPaymentIsNotCorrect() {
         MortgageCalculator myMortgageCalculator = new MortgageCalculator();
-
         myMortgageCalculator.setTypeOfPayment("NotCorrectType");
-
         assertEquals("NotCorrect",myMortgageCalculator.getTypeOfPayment());
     }
     @Test
-    public void geTypeOfPaymentIsNumber() {
+    public void checkTypeOfPaymentIsNumber() {
         MortgageCalculator myMortgageCalculator = new MortgageCalculator();
-
         myMortgageCalculator.setTypeOfPayment("0");
-
         assertEquals("NotCorrect",myMortgageCalculator.getTypeOfPayment());
+    }
+    @Test
+    public void checkTypeOfPaymentIsEmpty() {
+        MortgageCalculator myMortgageCalculator = new MortgageCalculator();
+        myMortgageCalculator.setTypeOfPayment("");
+        assertEquals("NotCorrect",myMortgageCalculator.getTypeOfPayment());
+    }
+    @Test
+    public void checkFullCostOfApartment() {
+        MortgageCalculator myMortgageCalculator = new MortgageCalculator();
+        myMortgageCalculator.setFullCostOfApartment(100000);
+        assertEquals(100000,myMortgageCalculator.getFullCostOfApartment(),epsilon);
+    }
+    @Test
+    public void checkFullCostOfApartmentIsZero() {
+        MortgageCalculator myMortgageCalculator = new MortgageCalculator();
+        myMortgageCalculator.setFullCostOfApartment(0);
+        assertEquals(-1,myMortgageCalculator.getFullCostOfApartment(),epsilon);
+    }
+    @Test
+    public void checkFullCostOfApartmentIsNegative() {
+        MortgageCalculator myMortgageCalculator = new MortgageCalculator();
+        myMortgageCalculator.setFullCostOfApartment(-300);
+        assertEquals(-1,myMortgageCalculator.getFullCostOfApartment(),epsilon);
     }
 }
