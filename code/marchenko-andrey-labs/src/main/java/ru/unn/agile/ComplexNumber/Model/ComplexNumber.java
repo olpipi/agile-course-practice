0b6895d918c3ffabb165826991bab3157fd664a2
Marchenko Andrey <andrey-95nn@yandex.ru>
2018-10-22 23:11:43 +0300

    Added initial structure

---
+++ code/marchenko-andrey-labs/src/main/java/ru/unn/agile/ComplexNumber/Model/ComplexNumber.java
@@ -0,0 +1,7 @@
+package ru.unn.agile.ComplexNumber.Model;
+
+import java.util.Objects;
+
+public class ComplexNumber {
+
+}
---
+++ code/marchenko-andrey-labs/src/test/java/ru/unn/agile/ComplexNumber/Model/ComplexNumberTest.java
@@ -0,0 +1,10 @@
+package ru.unn.agile.ComplexNumber.Model;
+
+import org.junit.Test;
+
+public class ComplexNumberTest {
+    @Test
+    public void test() {
+        assert true;
+    }
+}
