9e4b4b5493f07d62e0511ed91c29523238c622be
Podolov <kirill-podolov@yandex.ru>
2018-10-25 22:49:24 +0300

    Refactoring of tests

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/PodolovKirillTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/PodolovKirillTest.java
@@ -9,7 +9,9 @@ public class PodolovKirillTest {
     public void canAddComplexNumbers() {
         ComplexNumber z1 = new ComplexNumber(5, 1);
         ComplexNumber z2 = new ComplexNumber(1, 8);
+
         ComplexNumber sum = z1.add(z2);
+
         assertEquals(new ComplexNumber(6, 9), sum);
     }
 
@@ -17,7 +19,9 @@ public class PodolovKirillTest {
     public void canMultiplyComplexNumbers() {
         ComplexNumber z1 = new ComplexNumber(3, 7);
         ComplexNumber z2 = new ComplexNumber(8, 2);
+
         ComplexNumber mult = z1.multiply(z2);
+
         assertEquals(new ComplexNumber(10, 62), mult);
     }
 
@@ -25,6 +29,7 @@ public class PodolovKirillTest {
     public void areEquivalentComplexNumbersEquivalent() {
         ComplexNumber z1 = new ComplexNumber(5.28, 1.14);
         ComplexNumber z2 = new ComplexNumber(5.28, 1.14);
+
         assertTrue(z1.equals(z2));
     }
 }
