37dce7924672744a4613fe28e9bfbb20bdc39d21
Andrey Kochuev <kochuevai@gmail.com>
2018-10-25 15:13:45 +0300

    refactoring

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/KochuevAndreyTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/KochuevAndreyTest.java
@@ -7,31 +7,37 @@ public class KochuevAndreyTest {
     @Test
     public void canAddComplexNumbers() {
         // Arrange
-        ComplexNumber z1 = new ComplexNumber(1, 0);
-        ComplexNumber z2 = new ComplexNumber(0, 1);
+        ComplexNumber number1 = new ComplexNumber(1, 0);
+        ComplexNumber number2 = new ComplexNumber(0, 1);
+
         // Act
-        ComplexNumber z = z1.add(z2);
+        ComplexNumber summa = number1.add(number2);
+
         // Assert
-        assertEquals(new ComplexNumber(1, 1), z);
+        assertEquals(new ComplexNumber(1, 1), summa);
     }
 
     @Test
     public void canConvertToString() {
         // Arrange
-        ComplexNumber z = new ComplexNumber(1, 1);
+        ComplexNumber number = new ComplexNumber(1, 1);
+
         // Act
-        String zstr = z.toString();
+        String strNumber = number.toString();
+
         // Assert
-        assertEquals("1.0 + 1.0i", zstr);
+        assertEquals("1.0 + 1.0i", strNumber);
     }
 
     @Test
-    public void areNotEqualNumbersEqual() {
+    public void areNotEqualNumbers() {
         // Arrange
         ComplexNumber z1 = new ComplexNumber(1.02, 4.33);
         ComplexNumber z2 = new ComplexNumber(1.02, 4.73);
+
         // Act
         boolean isEqual = z1.equals(z2);
+
         // Assert
         assertFalse(isEqual);
     }
