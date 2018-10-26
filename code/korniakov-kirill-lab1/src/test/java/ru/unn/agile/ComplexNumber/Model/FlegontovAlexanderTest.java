d9efd980471716ba472e9baf7fe13d59bc94a516
alexanderflegontov <manshipnn@gmail.com>
2018-10-26 20:51:26 +0300

    minor fixes

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/FlegontovAlexanderTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/FlegontovAlexanderTest.java
@@ -8,15 +8,19 @@ public class FlegontovAlexanderTest {
     public void canAddNegativeComplexNumbers() {
         ComplexNumber c1 = new ComplexNumber(-1, -2);
         ComplexNumber c2 = new ComplexNumber(-3, -4);
-        ComplexNumber summa = c2.add(c1);
-        assertTrue(summa.equals(new ComplexNumber(-4, -6)));
+
+        ComplexNumber sum = c2.add(c1);
+
+        assertTrue(sum.equals(new ComplexNumber(-4, -6)));
     }
 
     @Test
     public void canMultComplexNumbers() {
         ComplexNumber c1 = new ComplexNumber(0.6, 0.33);
         ComplexNumber c2 = new ComplexNumber(8, 1);
+
         ComplexNumber mult = c2.multiply(c1);
+
         assertEquals(new ComplexNumber(4.47, 3.24), mult);
     }
 
@@ -24,6 +28,7 @@ public class FlegontovAlexanderTest {
     public void areComplexNumbersEquivalent() {
         ComplexNumber c1 = new ComplexNumber(1.1, 4.3);
         ComplexNumber c2 = new ComplexNumber(1.1, 4.3);
+
         assertTrue(c2.equals(c1));
     }
 }
