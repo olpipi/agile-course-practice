4d40bab1f2b23b2d7c2ea637505f46dfdbabe18f
zhiltsov-max <zhiltsov.max35@gmail.com>
2018-10-27 14:15:18 +0300

    Test for vectors of different length

--- code/vector-distance/src/test/java/ru/unn/agile/VectorDistance/Model/VectorDistanceTest.java
+++ code/vector-distance/src/test/java/ru/unn/agile/VectorDistance/Model/VectorDistanceTest.java
@@ -1,5 +1,6 @@
 package ru.unn.agile.vectordistance.model;
 
+import java.security.InvalidParameterException;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -12,5 +13,14 @@ public class VectorDistanceTest {
         float result = VectorDistance.computeL1(a, b);
 
         assertEquals(9.0f, result, 1e-14);
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    public void canNotComputeL1DistanceOfDifferentLengthVectors()
+            throws InvalidParameterException {
+        FloatVector a = new FloatVector(new float[]{1.0f});
+        FloatVector b = new FloatVector(new float[]{4.0f, 5.0f});
+
+        VectorDistance.computeL1(a, b);
     }
 }
