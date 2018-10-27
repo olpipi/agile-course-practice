b22286ca82d82c5479304a46b43702fde6cfbdeb
Ivan Magazinnik <magazinnik.ivan@gmail.com>
2018-10-27 13:36:07 +0300

    refator getNumbersArrayFromString move replace delimiter to add

--- code/string-calculator/src/main/java/ru/unn/agile/stringcalculator/model/StringCalculator.java
+++ code/string-calculator/src/main/java/ru/unn/agile/stringcalculator/model/StringCalculator.java
@@ -5,11 +5,13 @@ import java.util.Arrays;
 import java.util.List;
 
 public class StringCalculator {
-    private static final String DELIMITER = ",";
+    private static final String DEFAULT_DELIMITER = ",";
+    private String delimiter = DEFAULT_DELIMITER;
 
     public static int Add(String numbers) {
         int numbersSum = 0;
-        List<String> singleNumbers = getNumbersArrayFromString(numbers);
+        String numbersListWithDefaultDelimeter = replaceNewLines(numbers);
+        List<String> singleNumbers = getNumbersArrayFromString(numbersListWithDefaultDelimeter);
         for (String number : singleNumbers) {
             numbersSum += Integer.parseInt(number);
         }
@@ -18,20 +20,19 @@ public class StringCalculator {
 
     private static List<String> getNumbersArrayFromString(String numbersString) {
         List<String> numbersList = new ArrayList<>();
-        String numbersListWithDefaultDelimeter = replaceNewLines(numbersString);
-        if (!numbersListWithDefaultDelimeter.isEmpty()) {
-            if (numbersListWithDefaultDelimeter.contains(DELIMITER)) {
-                numbersList = Arrays.asList(numbersListWithDefaultDelimeter.split(DELIMITER));
+        if (!numbersString.isEmpty()) {
+            if (numbersString.contains(DEFAULT_DELIMITER)) {
+                numbersList = Arrays.asList(numbersString.split(DEFAULT_DELIMITER));
             }
             else {
-                numbersList.add(numbersListWithDefaultDelimeter);
+                numbersList.add(numbersString);
             }
         }
         return numbersList;
     }
 
     private static String replaceNewLines(String s) {
-        String replacedString = s.replace("\n", DELIMITER);
+        String replacedString = s.replace("\n", DEFAULT_DELIMITER);
         return replacedString;
     }
 }
