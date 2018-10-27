9c150a9a23e3583402af9d9fa96b2d02fca053a6
Ivan Magazinnik <magazinnik.ivan@gmail.com>
2018-10-27 11:54:49 +0300

    Add implementation for single number string

--- code/string-calculator/src/main/java/ru/unn/agile/stringcalculator/model/StringCalculator.java
+++ code/string-calculator/src/main/java/ru/unn/agile/stringcalculator/model/StringCalculator.java
@@ -3,6 +3,10 @@ package ru.unn.agile.stringcalculator.model;
 public class StringCalculator {
 
     public static int Add(String numbers) {
+        if (numbers.length() > 0)
+        {
+            return Integer.parseInt(numbers);
+        }
         return 0;
     }
 }
