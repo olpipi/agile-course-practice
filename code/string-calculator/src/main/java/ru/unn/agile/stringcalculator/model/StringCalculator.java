fa83a977abdff14c29667ef07ad9fb4ca43f75ea
Ivan Magazinnik <magazinnik.ivan@gmail.com>
2018-10-27 12:05:36 +0300

    add implimentation to add two numbers

--- code/string-calculator/src/main/java/ru/unn/agile/stringcalculator/model/StringCalculator.java
+++ code/string-calculator/src/main/java/ru/unn/agile/stringcalculator/model/StringCalculator.java
@@ -1,10 +1,19 @@
 package ru.unn.agile.stringcalculator.model;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StringCalculator {
 
     public static int Add(String numbers) {
         if (numbers.length() > 0)
         {
+            if (numbers.contains(","))
+            {
+                List<String> singleNumbers = Arrays.asList(numbers.split(","));
+                return Integer.parseInt(singleNumbers.get(0)) + Integer.parseInt(singleNumbers.get(1));
+            }
+
             return Integer.parseInt(numbers);
         }
         return 0;
