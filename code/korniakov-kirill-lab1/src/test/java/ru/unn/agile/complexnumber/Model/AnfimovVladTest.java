d6112ae9d51c40455f8d7402c7c55185b8f9c04a
Vladislav <vanfimov9@gmail.com>
2018-10-27 15:03:00 +0300

    Editing tests

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/complexnumber/Model/AnfimovVladTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/complexnumber/Model/AnfimovVladTest.java
@@ -5,25 +5,30 @@ import static org.junit.Assert.*;
 
 public class AnfimovVladTest {
     @Test
-    public void canEqualsNumbers() {
+    public void canEqualsDifferentComplexNumbers() {
         ComplexNumber z1 = new ComplexNumber(1, 2);
         ComplexNumber z2 = new ComplexNumber(2, 4);
+
         assertFalse(z1.equals(z2));
     }
 
     @Test
-    public void canAddNumbers() {
+    public void canAddComplexNumbers() {
         ComplexNumber z1 = new ComplexNumber(1, 2);
         ComplexNumber z2 = new ComplexNumber(2, 4);
+
         ComplexNumber z = z1.add(z2);
+
         assertEquals(new ComplexNumber(3, 6), z);
     }
 
     @Test
-    public void canMultiplyNumbers() {
+    public void canMultiplyComplexNumbers() {
         ComplexNumber z1 = new ComplexNumber(1, 2);
         ComplexNumber z2 = new ComplexNumber(2, 4);
+
         ComplexNumber z = z1.add(z2);
+
         assertEquals(new ComplexNumber(3, 6), z);
     }
 }
