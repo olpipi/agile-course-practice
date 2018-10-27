56b2500dc56ffb54fa3a2f33f336cda8820db2bf
alexanderflegontov <manshipnn@gmail.com>
2018-10-27 22:49:45 +0300

    Added tests for mean value Added the first implementation of mean value calculation

--- code/flegontov-alexander-math-statistics/src/main/java/ru/unn/agile/mathstatistics/model/MathStatistics.java
+++ code/flegontov-alexander-math-statistics/src/main/java/ru/unn/agile/mathstatistics/model/MathStatistics.java
@@ -1,4 +1,14 @@
 package ru.unn.agile.mathstatistics.model;
 
 public class MathStatistics {
+    static double MeanCalculate(Integer[] data)
+    {
+        double mean = 0;
+        for(int i=0; i < data.length; i++)
+        {
+            mean += data[i];
+        }
+        mean /= data.length;
+        return mean;
+    }
 }
--- code/flegontov-alexander-math-statistics/src/test/java/ru/unn/agile/mathstatistics/model/MathStatisticsTest.java
+++ code/flegontov-alexander-math-statistics/src/test/java/ru/unn/agile/mathstatistics/model/MathStatisticsTest.java
@@ -1,4 +1,37 @@
 package ru.unn.agile.mathstatistics.model;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MathStatisticsTest {
+
+    @Test
+    public void canCalculateMeanWithOneNumbers() {
+        Integer[] data = {10};
+
+        Double mean = MathStatistics.MeanCalculate(data);
+
+        assertEquals(new Double(10), mean);
+    }
+
+    @Test
+    public void canCalculateMeanWithOneNegotiveInteger() {
+        Integer[] data = {-23};
+
+        Double mean = MathStatistics.MeanCalculate(data);
+
+        assertEquals(new Double(-23), mean);
+    }
+
+    @Test
+    public void canCalculateMeanPositiveIntegers() {
+        Integer[] data = {10, 5, 25, 30};
+
+        Double mean = MathStatistics.MeanCalculate(data);
+
+        assertEquals(new Double(17.5), mean);
+    }
+
+
 }
