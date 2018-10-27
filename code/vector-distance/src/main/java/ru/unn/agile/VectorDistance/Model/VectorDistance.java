59daaa69ba13e3e3a2252f4fcff6ef00191f18a5
zhiltsov-max <zhiltsov.max35@gmail.com>
2018-10-27 19:06:24 +0300

    Refactored code for vector distance computation

--- code/vector-distance/src/main/java/ru/unn/agile/VectorDistance/Model/VectorDistance.java
+++ code/vector-distance/src/main/java/ru/unn/agile/VectorDistance/Model/VectorDistance.java
@@ -5,12 +5,8 @@ import java.util.Objects;
 
 public final class VectorDistance {
     public static float computeL1(final FloatVector a, final FloatVector b) {
-        Objects.requireNonNull(a, "Expected not null parameter a");
-        Objects.requireNonNull(b, "Expected not null parameter b");
-        if (a.length() != b.length()) {
-            throw new InvalidParameterException(
-                "Expected vectors of same length");
-        }
+        ensureNotNullArgs(a, b);
+        ensureSameLength(a, b);
 
         float distance = 0.f;
 
@@ -22,14 +18,31 @@ public final class VectorDistance {
     }
 
     public static float computeL2(final FloatVector a, final FloatVector b) {
+        ensureNotNullArgs(a, b);
+        ensureSameLength(a, b);
+
         float distance = 0.f;
 
         for (int i = 0; i < a.length(); ++i) {
-            distance += (a.get(i) - b.get(i)) * (a.get(i) - b.get(i));
+            distance += Math.pow(a.get(i) - b.get(i), 2);
         }
 
         return (float) Math.sqrt(distance);
     }
 
     private VectorDistance() { /* none */ }
+
+    private static void ensureNotNullArgs(
+            final FloatVector a, final FloatVector b) {
+        Objects.requireNonNull(a, "Expected not null parameter a");
+        Objects.requireNonNull(b, "Expected not null parameter b");
+    }
+
+    private static void ensureSameLength(
+            final FloatVector a, final FloatVector b) {
+        if (a.length() != b.length()) {
+            throw new InvalidParameterException(
+                "Expected vectors of same length");
+        }
+    }
 }
