e58713d8fc20ef2678cdea52d4ee31c2b5f9de53
Artem Shadrunov <artemshadrunov@gmail.com>
2018-10-28 21:52:09 +0300

    Upd: Constructor and tests

--- code/shadrunov-artem-bit-array/src/main/java/ru/unn/agile/bitarray/model/BitArray.java
+++ code/shadrunov-artem-bit-array/src/main/java/ru/unn/agile/bitarray/model/BitArray.java
@@ -1,4 +1,22 @@
 package ru.unn.agile.bitarray.model;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
+import java.util.BitSet;
+import java.util.List;
+
 public class BitArray {
+
+    private List<Integer> bits;
+
+    BitArray(List<Integer> bitArray) {
+        if (bitArray == null)
+            throw new NullPointerException();
+
+        if (bitArray.stream().anyMatch(i -> i < 0 || i > 1))
+            throw new IllegalArgumentException("Array must contains only 1 and 0");
+
+        this.bits = bitArray;
+    }
+
 }
--- code/shadrunov-artem-bit-array/src/test/java/ru/unn/agile/bitarray/model/BitArrayTests.java
+++ code/shadrunov-artem-bit-array/src/test/java/ru/unn/agile/bitarray/model/BitArrayTests.java
@@ -1,4 +1,41 @@
 package ru.unn.agile.bitarray.model;
 
+import org.junit.Test;
+import java.util.*;
+import static org.junit.Assert.assertNotNull;
+
+
 public class BitArrayTests {
+
+    @Test(expected = NullPointerException.class)
+    public void canInitializeWithNull() {
+        List<Integer> bits = null;
+
+        new BitArray(bits);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void canInitializeWithAnyDecimalNumbersArray(){
+        List<Integer> list = new ArrayList<>();
+
+        int i = 7;
+        while (i != 0)
+            list.add(i--);
+
+        new BitArray(list);
+    }
+
+    @Test
+    public void canCreateBitArrayWithOnesAndZeroesArray(){
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            list.add(1);
+            list.add(0);
+        }
+
+        BitArray bitArray = new BitArray(list);
+
+        assertNotNull(bitArray);
+    }
 }
