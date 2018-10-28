0f433ab89912cdca9c49722a90de4c1c6d55fd5a
olgaIvanova <olka-lyapa@mail.ru>
2018-10-29 00:01:12 +0300

    added tests for outputting one, two, three numbers

--- code/primeNumber/src/main/java/ru.unn.agile.primenumber.model/PrimeNumber.java
+++ code/primeNumber/src/main/java/ru.unn.agile.primenumber.model/PrimeNumber.java
@@ -1,8 +1,28 @@
 package ru.unn.agile.primenumber.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class PrimeNumber {
 
     private PrimeNumber() {
 
+    }
+
+    public static List<Integer> findPrimeNumbers(final int firstValueInterval,
+                                                 final int lastValueInterval) {
+        List<Integer> primeNumbersArray = new ArrayList<>();
+
+            for (int i = 2; i <= lastValueInterval; i++) {
+                if (i == 2) {
+                    primeNumbersArray.add(i);
+                }
+
+                if (i % 2 != 0) {
+                    primeNumbersArray.add(i);
+                }
+            }
+
+        return primeNumbersArray;
     }
 }
--- code/primeNumber/src/test/java/ru.unn.agile.primenumber.model/PrimeNumberTest.java
+++ code/primeNumber/src/test/java/ru.unn.agile.primenumber.model/PrimeNumberTest.java
@@ -2,10 +2,54 @@ package ru.unn.agile.primenumber.model;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+
+import java.util.List;
 
 public final class PrimeNumberTest {
 
     @Test
-    public void firstTest() {
+    public void canReceiveOnePrimeValueFromIntervalZeroToTwo() {
+        int firstValueInterval = 0;
+        int lastValueInterval = 2;
+
+        List<Integer> expectedArray = new ArrayList<>();
+        expectedArray.add(2);
+
+        List<Integer> actualArray =
+                PrimeNumber.findPrimeNumbers(firstValueInterval, lastValueInterval);
+
+        assertEquals(expectedArray, actualArray);
+    }
+
+    @Test
+    public void canReceiveTwoPrimeValueFromIntervalZeroToThree() {
+        int firstValueInterval = 0;
+        int lastValueInterval = 3;
+
+        List<Integer> expectedArray = new ArrayList<>();
+        expectedArray.add(2);
+        expectedArray.add(3);
+
+        List<Integer> actualArray =
+                PrimeNumber.findPrimeNumbers(firstValueInterval, lastValueInterval);
+
+        assertEquals(expectedArray, actualArray);
+    }
+
+    @Test
+    public void canReceiveThreePrimeValueFromIntervalZeroToFive() {
+        int firstValueInterval = 0;
+        int lastValueInterval = 5;
+
+        List<Integer> expectedArray = new ArrayList<>();
+        expectedArray.add(2);
+        expectedArray.add(3);
+        expectedArray.add(5);
+
+        List<Integer> actualArray =
+                PrimeNumber.findPrimeNumbers(firstValueInterval, lastValueInterval);
+
+        assertEquals(expectedArray, actualArray);
     }
 }
