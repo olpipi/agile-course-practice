7b294dcf9ecf929c4b6937b2da5b9129f4dc6ad1
TanyaGladkova <tanya_gladkova@list.ru>
2018-10-28 22:41:31 +0300

    moved search test

--- code/gladkova-tatiana-binary-search/src/test/java/ru/unn/agile/number/binarysearch/model/BinarySearchTests.java
+++ code/gladkova-tatiana-binary-search/src/test/java/ru/unn/agile/number/binarysearch/model/BinarySearchTests.java
@@ -70,19 +70,19 @@ public class BinarySearchTests {
     }
 
     @Test
-    public void canSearchFirstKeyInArray() {
-        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int key = 0;
-        BinarySearch binarySearch = new BinarySearch(array, key);
-        assertEquals(0, binarySearch.doSearch());
-    }
-
-    @Test
     public void canSearchMiddleKeyInArray() {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int key = 5;
         BinarySearch binarySearch = new BinarySearch(array, key);
         assertEquals(4, binarySearch.doSearch());
+    }
+
+    @Test
+    public void canSearchFirstKeyInArray() {
+        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int key = 0;
+        BinarySearch binarySearch = new BinarySearch(array, key);
+        assertEquals(0, binarySearch.doSearch());
     }
 
     @Test
