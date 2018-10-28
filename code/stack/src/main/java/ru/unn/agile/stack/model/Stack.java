37fcdd022bc4743c8a8db63e1f5427276a92bf57
1995konstantin <1995kostia@mail.ru>
2018-10-28 16:37:11 +0300

    Added push() and pop() methods

--- code/stack/src/main/java/ru/unn/agile/stack/model/Stack.java
+++ code/stack/src/main/java/ru/unn/agile/stack/model/Stack.java
@@ -7,10 +7,30 @@ public class Stack<E> {
     private List<E> list;
 
     public Stack() {
-        list = new ArrayList<E>();
+        list = new ArrayList<>();
     }
 
     public boolean empty() {
         return list.size() == 0;
     }
+
+    public E push(E item) {
+        if (item == null) {
+            throw new IllegalArgumentException("Element must not be null");
+        }
+
+        list.add(item);
+        return item;
+    }
+
+    public synchronized E pop() {
+        int size = list.size();
+
+        if (size < 1) {
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+
+        return list.remove(size - 1);
+    }
+
 }
--- code/stack/src/test/java/ru/unn/agile/stack/model/StackTest.java
+++ code/stack/src/test/java/ru/unn/agile/stack/model/StackTest.java
@@ -6,9 +6,45 @@ import static org.junit.Assert.*;
 
 public class StackTest {
     @Test
-    public void canCreateEmptyStack() {
+    public void canCreateStack() {
         Stack stack = new Stack<Integer>();
 
         assertTrue(stack.empty());
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void canNotPushNullToStack() {
+        Stack stack = new Stack<Integer>();
+
+        stack.push(null);
+    }
+
+    @Test
+    public void canPushObjectToStack() {
+        Object expected = new Object();
+        Stack stack = new Stack<Integer>();
+
+        Object actual = stack.push(expected);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void canNotPopElementFromEmptyStack() {
+        Stack stack = new Stack<Integer>();
+
+        stack.pop();
+    }
+
+    @Test
+    public void canPopElementFromNotEmptyStack() {
+        Object expected = new Object();
+        Stack stack = new Stack<Integer>();
+        stack.push(expected);
+
+        Object actual = stack.pop();
+
+        assertEquals(expected, actual);
+    }
+
 }
