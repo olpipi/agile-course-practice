a4003431d047679cd3a40cd68fd6561d18979257
Artem Shadrunov <artemshadrunov@gmail.com>
2018-10-28 23:07:14 +0300

    Upd: Added toString() and test

--- code/shadrunov-artem-bit-array/src/main/java/ru/unn/agile/bitarray/model/BitArray.java
+++ code/shadrunov-artem-bit-array/src/main/java/ru/unn/agile/bitarray/model/BitArray.java
@@ -2,12 +2,17 @@ package ru.unn.agile.bitarray.model;
 
 import com.sun.javaws.exceptions.InvalidArgumentException;
 
+import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.List;
 
 public class BitArray {
 
     private List<Integer> bits;
+
+    BitArray(){
+        this.bits = new ArrayList<>();
+    }
 
     BitArray(List<Integer> bitArray) {
         if (bitArray == null)
@@ -16,7 +21,18 @@ public class BitArray {
         if (bitArray.stream().anyMatch(i -> i < 0 || i > 1))
             throw new IllegalArgumentException("Array must contains only 1 and 0");
 
+        int i = 0;
+        while (bitArray.get(i) == 0)
+            bitArray.remove(i);
+
         this.bits = bitArray;
     }
 
+    @Override
+    public String toString() {
+        String stringBitArray = "";
+        for (Integer i: bits) stringBitArray += i.toString();
+
+        return stringBitArray;
+    }
 }
--- code/shadrunov-artem-bit-array/src/test/java/ru/unn/agile/bitarray/model/BitArrayTests.java
+++ code/shadrunov-artem-bit-array/src/test/java/ru/unn/agile/bitarray/model/BitArrayTests.java
@@ -2,6 +2,8 @@ package ru.unn.agile.bitarray.model;
 
 import org.junit.Test;
 import java.util.*;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 
@@ -37,5 +39,39 @@ public class BitArrayTests {
         BitArray bitArray = new BitArray(list);
 
         assertNotNull(bitArray);
+    }
+
+    @Test
+    public void canConvertToString(){
+        List<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        for (int i = 0; i < 3; i++)
+            list.add(0);
+
+        BitArray bitArray = new BitArray(list);
+
+        assertEquals("1000", bitArray.toString());
+    }
+
+    @Test
+    public void canConvertEmptyBitArrayToStrng(){
+        BitArray bitArray = new BitArray();
+
+        assertEquals("", bitArray.toString());
+    }
+
+    @Test
+    public void canCutFirstZeroesInStringRepresentation(){
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++)
+            list.add(0);
+
+        list.add(1);
+
+        BitArray bitArray = new BitArray(list);
+
+        assertEquals("1", bitArray.toString());
     }
 }
