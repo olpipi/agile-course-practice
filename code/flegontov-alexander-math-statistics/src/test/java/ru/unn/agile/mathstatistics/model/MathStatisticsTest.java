1e687e33a9209f43e7274d8602b89c40a7d52823
alexanderflegontov <manshipnn@gmail.com>
2018-10-28 10:20:46 +0300

    added tests for dispersion calculation

--- code/flegontov-alexander-math-statistics/src/test/java/ru/unn/agile/mathstatistics/model/MathStatisticsTest.java
+++ code/flegontov-alexander-math-statistics/src/test/java/ru/unn/agile/mathstatistics/model/MathStatisticsTest.java
@@ -83,4 +83,62 @@ public class MathStatisticsTest {
     }
 
 
+    @Test
+    public void canCalculateDispersionWithOneNumber() {
+        Integer[] data = {8};
+
+        Double dispersion = MathStatistics.dispersionCalculate(data);
+
+        assertEquals(new Double(0), dispersion);
+    }
+
+    @Test
+    public void canCalculateDispersionWithOneNegativeNumber() {
+        Integer[] data = {-23};
+
+        Double dispersion = MathStatistics.dispersionCalculate(data);
+
+        assertEquals(new Double(0), dispersion);
+    }
+
+    @Test
+    public void canCalculateDispersionWithNegativeNumbers() {
+        Number[] data = {9, 3, -1, 9, -8};
+
+        Double dispersion = MathStatistics.dispersionCalculate(data);
+
+        assertEquals(new Double(41.44), dispersion);
+    }
+
+    @Test
+    public void canCalculateDispersionWithArrayofZeros() {
+        Number[] data = {0, 0, 0, 0};
+
+        Double dispersion = MathStatistics.dispersionCalculate(data);
+
+        assertEquals(new Double(0.0), dispersion);
+    }
+
+    @Test
+    public void canCalculateDispersionWithDouble() {
+        Number[] data = {7.2, 64.3, -13.8, 0.6, 23.1};
+
+        Double dispersion = MathStatistics.dispersionCalculate(data);
+
+        assertEquals(new Double(717.1096), dispersion);
+    }
+
+    @Test
+    public void canCalculateDispersionWithBigSizeofDataArray() {
+        Integer[] data = new Integer[1000];
+        for (int i = 0; i < data.length; ++i) {
+            data[i] = i;
+        }
+
+        Double dispersion = MathStatistics.dispersionCalculate(data);
+
+        assertEquals(new Double(83333.25), dispersion);
+    }
+
+
 }
