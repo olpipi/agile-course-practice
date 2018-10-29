d0ea5a64fe656519534ad0f59617e658ac73fd9e
Oleg Chekodaev <ch.oleg@hotmail.com>
2018-10-29 12:48:54 +0300

    Refactor

--- code/chekodaev-oleg-roman-number-converter/src/main/java/ru/unn/agile/romannumberconverter/model/RomanNumberConverter.java
+++ code/chekodaev-oleg-roman-number-converter/src/main/java/ru/unn/agile/romannumberconverter/model/RomanNumberConverter.java
@@ -1,30 +1,28 @@
 package ru.unn.agile.romannumberconverter.model;
 
-public final class RomanNumberConverter {
-	private static String convertToRomanFirstThreeNumbers(final int arabicNumber) {
-		String romanNumber = "";
-		if (arabicNumber < 4) {
-			for (int i = 0; i < arabicNumber; i++) {
-				romanNumber += "I";
-			}
-		}
+import java.util.Map;
+import java.util.TreeMap;
 
-		return romanNumber;
-	}
+public final class RomanNumberConverter {
+    private static final Map<Integer, String> BASE_NUMBER_MAP = new TreeMap<Integer, String>();
+    static {
+        BASE_NUMBER_MAP.put(1, "I");
+        BASE_NUMBER_MAP.put(4, "IV");
+        BASE_NUMBER_MAP.put(5, "V");
+        BASE_NUMBER_MAP.put(9, "IX");
+        BASE_NUMBER_MAP.put(10, "X");
+    }
 
     public static String convertToRoman(final int arabicNumber) {
-		String romanNumber = "";
-		if (arabicNumber < 4) {
-			romanNumber = convertToRomanFirstThreeNumbers(arabicNumber);
-		} else if (arabicNumber == 4) {
-			romanNumber = "IV";
-		} else if (arabicNumber >= 5 && arabicNumber < 9) {
-			romanNumber = "V";
-			romanNumber += convertToRomanFirstThreeNumbers(arabicNumber - 5);
-		} else if (arabicNumber == 9) {
-            romanNumber = "IX";
+        String romanNumber = "";
+        int nearestKey = ((TreeMap<Integer, String>) BASE_NUMBER_MAP).floorKey(arabicNumber);
+        if (nearestKey == arabicNumber) {
+            romanNumber = BASE_NUMBER_MAP.get(arabicNumber);
+        } else {
+            romanNumber = BASE_NUMBER_MAP.get(nearestKey)
+                    + convertToRoman(arabicNumber - nearestKey);
         }
-			
-		return romanNumber;
+
+        return romanNumber;
     }
 }
