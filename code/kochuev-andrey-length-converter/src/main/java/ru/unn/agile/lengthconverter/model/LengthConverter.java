8a89d2082a4801cc29c87e6d01fe3453f353dcf5
Andrey Kochuev <kochuevai@gmail.com>
2018-10-29 23:00:29 +0300

    first test

--- code/kochuev-andrey-length-converter/src/main/java/ru/unn/agile/lengthconverter/model/LengthConverter.java
+++ code/kochuev-andrey-length-converter/src/main/java/ru/unn/agile/lengthconverter/model/LengthConverter.java
@@ -1,5 +1,8 @@
 package ru.unn.agile.lengthconverter.model;
 
 public class LengthConverter {
-
+    public final double convert(final double value)
+    {
+        return value*1000;
+    }
 }
--- code/kochuev-andrey-length-converter/src/test/java/ru/unn/agile/lengthconverter/model/LengthConverterTest.java
+++ code/kochuev-andrey-length-converter/src/test/java/ru/unn/agile/lengthconverter/model/LengthConverterTest.java
@@ -5,8 +5,12 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class LengthConverterTest {
+    private static final double EPSILON = 0.000001;
+
     @Test
-    public void justTest() {
-        assertEquals(1, 1);
+    public void canConvertMetersToMillimeters() {
+        LengthConverter converter = new LengthConverter();
+        double convertedValue = converter.convert(5);
+        assertEquals(5000, convertedValue,EPSILON);
     }
 }
