2e8323cfaef54fd4534b17db962c0ae3850af810
KryukovaPolina <paulina-17@yandex.ru>
2018-10-29 21:44:40 +0300

    added basic encode method

---
+++ code/caesar-cipher/src/main/java/ru/unn/agile/caesarcipher/model/CaesarCipher.java
@@ -0,0 +1,12 @@
+package ru.unn.agile.caesarcipher.model;
+
+public class CaesarCipher {
+    public CaesarCipher() {
+        /* constructor not needed here */
+    }
+
+    public String encode(final String input, final int offset) {
+        return input;
+    }
+
+}
