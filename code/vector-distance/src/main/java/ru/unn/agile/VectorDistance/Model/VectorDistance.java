d2c2d8720634d983060e233dc82379c3a55472e1
zhiltsov-max <zhiltsov.max35@gmail.com>
2018-10-29 22:44:40 +0300

    Refactored L1-L4 distances

--- code/vector-distance/src/main/java/ru/unn/agile/VectorDistance/Model/VectorDistance.java
+++ code/vector-distance/src/main/java/ru/unn/agile/VectorDistance/Model/VectorDistance.java
@@ -4,70 +4,42 @@ import java.security.InvalidParameterException;
 import java.util.Objects;
 
 public final class VectorDistance {
-    public static float computeL1(final FloatVector a, final FloatVector b) {
-        ensureNotNullArgs(a, b);
-        ensureSameLength(a, b);
+    private static float computeLPdistance(
+            final FloatVector a, final FloatVector b, final double p) {
+        checkArgs(a, b);
 
         float distance = 0.0f;
-
         for (int i = 0; i < a.length(); ++i) {
-            distance += Math.abs(a.get(i) - b.get(i));
+            distance += Math.pow(Math.abs(a.get(i) - b.get(i)), p);
             ensureIsFinite(distance);
         }
+        return (float) Math.pow(distance, 1.0 / p);
+    }
 
-        return distance;
+    public static float computeL1(final FloatVector a, final FloatVector b) {
+        final double p = 1.0;
+        return computeLPdistance(a, b, p);
     }
 
     public static float computeL2(final FloatVector a, final FloatVector b) {
-        ensureNotNullArgs(a, b);
-        ensureSameLength(a, b);
-
-        float distance = 0.0f;
-
-        for (int i = 0; i < a.length(); ++i) {
-            distance += Math.pow(a.get(i) - b.get(i), 2);
-            ensureIsFinite(distance);
-        }
-
-        return (float) Math.sqrt(distance);
+        final double p = 2.0;
+        return computeLPdistance(a, b, p);
     }
 
     public static float computeL3(final FloatVector a, final FloatVector b) {
-        ensureNotNullArgs(a, b);
-        ensureSameLength(a, b);
-
-        final double l3Power = 3.0;
-        float distance = 0.0f;
-
-        for (int i = 0; i < a.length(); ++i) {
-            distance += Math.pow(Math.abs(a.get(i) - b.get(i)), l3Power);
-            ensureIsFinite(distance);
-        }
-
-        return (float) Math.pow(distance, 1.0 / l3Power);
+        final double p = 3.0;
+        return computeLPdistance(a, b, p);
     }
 
     public static float computeL4(final FloatVector a, final FloatVector b) {
-        ensureNotNullArgs(a, b);
-        ensureSameLength(a, b);
-
-        final double l4Power = 4.0;
-        float distance = 0.0f;
-
-        for (int i = 0; i < a.length(); ++i) {
-            distance += Math.pow(a.get(i) - b.get(i), l4Power);
-            ensureIsFinite(distance);
-        }
-
-        return (float) Math.pow(distance, 1.0 / l4Power);
+        final double p = 4.0;
+        return computeLPdistance(a, b, p);
     }
 
     public static float computeLinf(final FloatVector a, final FloatVector b) {
-        ensureNotNullArgs(a, b);
-        ensureSameLength(a, b);
+        checkArgs(a, b);
 
         float distance = 0.0f;
-
         for (int i = 0; i < a.length(); ++i) {
             float currentDistance = Math.abs(a.get(i) - b.get(i));
             ensureIsFinite(currentDistance);
@@ -75,7 +47,6 @@ public final class VectorDistance {
                 distance = currentDistance;
             }
         }
-
         return distance;
     }
 
@@ -87,14 +58,9 @@ public final class VectorDistance {
         }
     }
 
-    private static void ensureNotNullArgs(
-            final FloatVector a, final FloatVector b) {
+    private static void checkArgs(final FloatVector a, final FloatVector b) {
         Objects.requireNonNull(a, "Expected not null parameter a");
         Objects.requireNonNull(b, "Expected not null parameter b");
-    }
-
-    private static void ensureSameLength(
-            final FloatVector a, final FloatVector b) {
         if (a.length() != b.length()) {
             throw new InvalidParameterException(
                 "Expected vectors of same length");
