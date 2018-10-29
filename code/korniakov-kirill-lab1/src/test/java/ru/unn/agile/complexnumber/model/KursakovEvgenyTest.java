6f276db129a648e79c6c18bdc21e23b6f208cf31
Evgeny Kursakov <ekursakov@yandex.ru>
2018-10-29 19:39:11 +0300

    [lab 0] shortened variable names

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/complexnumber/model/KursakovEvgenyTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/complexnumber/model/KursakovEvgenyTest.java
@@ -7,10 +7,10 @@ import static org.junit.Assert.assertEquals;
 public class KursakovEvgenyTest {
     @Test
     public void canAddRealComplexNumbers() {
-        ComplexNumber number1 = new ComplexNumber(2, 0);
-        ComplexNumber number2 = new ComplexNumber(2, 0);
+        ComplexNumber z1 = new ComplexNumber(2, 0);
+        ComplexNumber z2 = new ComplexNumber(2, 0);
 
-        ComplexNumber result = number1.add(number2);
+        ComplexNumber result = z1.add(z2);
 
         ComplexNumber expected = new ComplexNumber(4, 0);
         assertEquals(expected, result);
@@ -18,10 +18,10 @@ public class KursakovEvgenyTest {
 
     @Test
     public void canMultiplyComplexNumbers() {
-        ComplexNumber number1 = new ComplexNumber(1.5, -2.5);
-        ComplexNumber number2 = new ComplexNumber(-1, 3);
+        ComplexNumber z1 = new ComplexNumber(1.5, -2.5);
+        ComplexNumber z2 = new ComplexNumber(-1, 3);
 
-        ComplexNumber result = number1.multiply(number2);
+        ComplexNumber result = z1.multiply(z2);
 
         ComplexNumber expected = new ComplexNumber(6, 7);
         assertEquals(expected, result);
@@ -29,10 +29,10 @@ public class KursakovEvgenyTest {
 
     @Test
     public void canMultiplyWithZero() {
-        ComplexNumber number1 = new ComplexNumber(100, 200);
-        ComplexNumber number2 = new ComplexNumber(0, 0);
+        ComplexNumber z1 = new ComplexNumber(100, 200);
+        ComplexNumber z2 = new ComplexNumber(0, 0);
 
-        ComplexNumber result = number1.multiply(number2);
+        ComplexNumber result = z1.multiply(z2);
 
         ComplexNumber expected = new ComplexNumber(0, 0);
         assertEquals(expected, result);
