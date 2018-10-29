c7004d5f441298f69bdb06db69edc45538b36a4f
Oleg Chekodaev <ch.oleg@hotmail.com>
2018-10-29 12:44:30 +0300

    Refactor

--- code/chekodaev-oleg-roman-number-converter/src/main/java/ru/unn/agile/romannumberconverter/model/RomanNumberConverter.java
+++ code/chekodaev-oleg-roman-number-converter/src/main/java/ru/unn/agile/romannumberconverter/model/RomanNumberConverter.java
@@ -1,21 +1,26 @@
 package ru.unn.agile.romannumberconverter.model;
 
 public final class RomanNumberConverter {
-    public static String convertToRoman(final int arabicNumber) {
+	private static String convertToRomanFirstThreeNumbers(final int arabicNumber) {
 		String romanNumber = "";
 		if (arabicNumber < 4) {
 			for (int i = 0; i < arabicNumber; i++) {
 				romanNumber += "I";
 			}
+		}
+
+		return romanNumber;
+	}
+
+    public static String convertToRoman(final int arabicNumber) {
+		String romanNumber = "";
+		if (arabicNumber < 4) {
+			romanNumber = convertToRomanFirstThreeNumbers(arabicNumber);
 		} else if (arabicNumber == 4) {
 			romanNumber = "IV";
-		} else if (arabicNumber == 5) {
+		} else if (arabicNumber >= 5) {
 			romanNumber = "V";
-		} else if (arabicNumber > 5) {
-			romanNumber = "V";
-			for (int i = 0; i < arabicNumber - 5; i++) {
-				romanNumber += "I";
-			}
+			romanNumber += convertToRomanFirstThreeNumbers(arabicNumber - 5);
 		}
 			
 		return romanNumber;
