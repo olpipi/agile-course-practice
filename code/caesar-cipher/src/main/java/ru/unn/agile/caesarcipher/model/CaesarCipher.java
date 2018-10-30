6b8995365194983ece14f607d40b3e65dc274ebf
KryukovaPolina <paulina-17@yandex.ru>
2018-10-30 22:25:30 +0300

    extended encode metod to pass wraparound tests

--- code/caesar-cipher/src/main/java/ru/unn/agile/caesarcipher/model/CaesarCipher.java
+++ code/caesar-cipher/src/main/java/ru/unn/agile/caesarcipher/model/CaesarCipher.java
@@ -5,11 +5,20 @@ public class CaesarCipher {
         /* constructor not needed here */
     }
 
+    private int numberOfCharactersInInterval = (int) 'z' - (int) 'a' + 1;
+    private int startingPosition = (int) 'a';
+
     public String encode(final String input, final int offset) {
         String output = "";
         for (int i = 0; i < input.length(); i++) {
-            int charCode = (int) input.charAt(i);
-            output += (char) (charCode + offset);
+            int charCode = (int) input.charAt(i) - startingPosition;
+            int newCharCode = charCode + offset;
+            if (newCharCode < 0) {
+                newCharCode += numberOfCharactersInInterval;
+            }
+            newCharCode %= numberOfCharactersInInterval;
+            newCharCode += startingPosition;
+            output += (char) (newCharCode);
         }
         return output;
     }
