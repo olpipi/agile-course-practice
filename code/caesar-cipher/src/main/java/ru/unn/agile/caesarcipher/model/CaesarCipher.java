3a29a17fc5996b6ce466487aa9c2b3ea18fe11d5
KryukovaPolina <paulina-17@yandex.ru>
2018-10-31 00:24:14 +0300

    encode capital letters support

--- code/caesar-cipher/src/main/java/ru/unn/agile/caesarcipher/model/CaesarCipher.java
+++ code/caesar-cipher/src/main/java/ru/unn/agile/caesarcipher/model/CaesarCipher.java
@@ -6,12 +6,23 @@ public class CaesarCipher {
     }
 
     private int numberOfCharactersInInterval = (int) 'z' - (int) 'a' + 1;
-    private int startingPosition = (int) 'a';
+    //private int startingPosition = (int) 'a';
+    private int calculateStartingPosition(final int charCode) {
+        int startingPosition = 0;
+        if ((charCode >= (int) 'a') & (charCode <= (int) 'z')) {
+            startingPosition = (int) 'a';
+        } else if ((charCode >= (int) 'A') & (charCode <= (int) 'Z')) {
+            startingPosition = (int) 'A';
+        }
+        return startingPosition;
+    }
 
     public String encode(final String input, final int offset) {
         String output = "";
         for (int i = 0; i < input.length(); i++) {
-            int charCode = (int) input.charAt(i) - startingPosition;
+            int charCode = (int) input.charAt(i);
+            int startingPosition = calculateStartingPosition(charCode);
+            charCode -= startingPosition;
             int newCharCode = charCode + offset;
             if (newCharCode < 0) {
                 newCharCode += numberOfCharactersInInterval;
