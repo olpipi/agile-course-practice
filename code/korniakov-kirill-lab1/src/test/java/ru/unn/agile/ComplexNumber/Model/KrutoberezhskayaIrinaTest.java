a2d271043b8ba5f800a9c7b2db49db36ec6dba6f
Irina20106 <irina20106@list.ru>
2018-10-31 20:57:43 +0300

    KrutoberezhskayaIrina

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/KrutoberezhskayaIrinaTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/KrutoberezhskayaIrinaTest.java
@@ -2,7 +2,7 @@ package ru.unn.agile.complexnumber.model;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class KrutoberezhskayaIrinaTest {
 
