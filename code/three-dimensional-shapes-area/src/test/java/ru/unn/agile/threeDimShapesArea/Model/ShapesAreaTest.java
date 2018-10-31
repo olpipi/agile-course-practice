821aa9376a32975c94b0e53bca96b2f9d6f9a761
Mariia Pronina <essa.ae.mirr@gmail.com>
2018-10-31 20:26:15 +0300

    refactored tests

--- code/three-dimensional-shapes-area/src/test/java/ru/unn/agile/threeDimShapesArea/Model/ShapesAreaTest.java
+++ code/three-dimensional-shapes-area/src/test/java/ru/unn/agile/threeDimShapesArea/Model/ShapesAreaTest.java
@@ -3,125 +3,186 @@ package ru.unn.agile.threeDimShapesArea.Model;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ShapesAreaTest {
+    private static final double DELTA = 0.00001;
+
     @Test
     public void canGetCubeArea() {
+        // Arrange
         Shape cube = new Cube(1);
+        // Act
         double area = cube.getArea();
-        assert area == 6;
+        // Assert
+        double expectedArea = 6;
+        assertEquals(expectedArea, area, DELTA);
     }
 
     @Test
     public void canGetCubeAreaWithTwoSide() {
+        // Arrange
         Shape cube = new Cube(2);
+        // Act
         double area = cube.getArea();
-        assert area == 24;
+        // Assert
+        double expectedArea = 24;
+        assertEquals(expectedArea, area, DELTA);
     }
 
     @Test
     public void canGetSphereArea() {
+        // Arrange
         Shape sphere = new Sphere(1);
+        // Act
         double area = sphere.getArea();
-        assert area == 4 * Math.PI;
+        // Assert
+        double expectedArea = 4 * Math.PI;
+        assertEquals(expectedArea, area, DELTA);
     }
 
     @Test
     public void canGetSphereWithRadiusTwoArea() {
+        // Arrange
         Shape sphere = new Sphere(2);
+        // Act
         double area = sphere.getArea();
-        assert area == 16 * Math.PI;
+        // Assert
+        double expectedArea = 16 * Math.PI;
+        assertEquals(expectedArea, area, DELTA);
     }
 
     @Test
     public void canGetUnitParallelepipedArea() {
+        // Arrange
         Shape parallelepiped = new Parallelepiped(1, 1, 1);
+        // Act
         double area = parallelepiped.getArea();
-        assert area == 6;
+        // Assert
+        double expectedArea = 6;
+        assertEquals(expectedArea, area, DELTA);
     }
 
     @Test
     public void canGetParallelepipedArea() {
+        // Arrange
         Shape parallelepiped = new Parallelepiped(1, 3, 5);
+        // Act
         double area = parallelepiped.getArea();
-        assert area == 46;
+        // Assert
+        double expectedArea = 46;
+        assertEquals(expectedArea, area, DELTA);
     }
 
 
     @Test
     public void canGetUnitCylinderArea() {
+        // Arrange
         Shape cylinder = new Cylinder(1, 1);
+        // Act
         double area = cylinder.getArea();
-        assert area == 4 * Math.PI;
+        // Assert
+        double expectedArea = 4 * Math.PI;
+        assertEquals(expectedArea, area, DELTA);
     }
 
     @Test
     public void canGetCylinderArea() {
+        // Arrange
         Shape cylinder = new Cylinder(3, 5);
+        // Act
         double area = cylinder.getArea();
-        assert area == 2 * Math.PI * 3 * 8;
+        // Assert
+        double expectedArea = 2 * Math.PI * 3 * 8;
+        assertEquals(expectedArea, area, DELTA);
     }
 
     @Test
     public void canGetUnitSquarePyramidArea() {
+        // Arrange
         Shape squarePyramid = new SquarePyramid(1, 1);
+        // Act
         double area = squarePyramid.getArea();
-        assert area == 1 + 2 * Math.sqrt(0.75);
+        // Assert
+        double expectedArea = 1 + 2 * Math.sqrt(0.75);
+        assertEquals(expectedArea, area, DELTA);
     }
 
     @Test
     public void canGetSquarePyramidArea() {
+        // Arrange
         Shape squarePyramid = new SquarePyramid(2, 5);
+        // Act
         double area = squarePyramid.getArea();
+        // Assert
         double expectedArea = 4 + 2 * 2 * Math.sqrt(5 * 5 - 2 * 2 / 4);
-        assert Math.abs(area - expectedArea) < 0.00001;
+        assertEquals(expectedArea, area, DELTA);
     }
 
     @Test
     public void canGetUnitHexagonalPyramidArea() {
+        // Arrange
         Shape pyramid = new HexagonalPyramid(1, 1);
+        // Act
         double area = pyramid.getArea();
+        // Assert
         double expectedArea = 3 * (Math.sqrt(0.75) + Math.sqrt(3) / 2);
-        assert Math.abs(area - expectedArea) < 0.00001;
+        assertEquals(expectedArea, area, DELTA);
     }
 
     @Test
     public void canGetUnitTriangleArea() {
+        // Arrange
         Shape triangle = new Triangle(1, 1, 1);
+        // Act
         double area = triangle.getArea();
+        // Assert
         double expectedArea = Math.sqrt(3) / 4;
-        assert Math.abs(area - expectedArea) < 0.00001;
+        assertEquals(expectedArea, area, DELTA);
     }
 
     @Test
     public void canGetUnitHexagonArea() {
+        // Arrange
         Shape hexagon = new Hexagon(1);
+        // Act
         double area = hexagon.getArea();
+        // Assert
         double expectedArea = 3 * Math.sqrt(3) / 2;
-        assert Math.abs(area - expectedArea) < 0.00001;
+        assertEquals(expectedArea, area, DELTA);
     }
 
     @Test
     public void canGetTrianglePrismArea() {
+        // Arrange
         Shape prism = new TrianglePrism(new Triangle(1, 1, 1), 5);
+        // Act
         double area = prism.getArea();
+        // Assert
         double expectedArea = Math.sqrt(3) / 2 + 3 * 5;
-        assert Math.abs(area - expectedArea) < 0.00001;
+        assertEquals(expectedArea, area, DELTA);
     }
 
     @Test
     public void canGetCircleArea() {
+        // Arrange
         Shape circle = new Circle(4);
+        // Act
         double area = circle.getArea();
+        // Assert
         double expectedArea = Math.PI * 16;
-        assert Math.abs(area - expectedArea) < 0.00001;
+        assertEquals(expectedArea, area, DELTA);
     }
 
     @Test
     public void canGetConesArea() {
+        // Arrange
         Shape cones = new Cone(4, 5);
+        // Act
         double area = cones.getArea();
+        // Assert
         double expectedArea = Math.PI * 20 + Math.PI * 16;
-        assert Math.abs(area - expectedArea) < 0.00001;
+        assertEquals(expectedArea, area, DELTA);
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -176,19 +237,25 @@ public class ShapesAreaTest {
 
     @Test
     public void canGetTruncatedConeArea() {
+        // Arrange
         Shape cone = new TruncatedCone(2, 4, 1);
+        // Act
         double area = cone.getArea();
+        // Assert
         double expectedArea = Math.PI * (2 + 4) * 1 + Math.PI * 4 + Math.PI * 16;
-        assert Math.abs(area - expectedArea) < 0.00001;
+        assertEquals(expectedArea, area, DELTA);
     }
 
     @Test
     public void canGetTrianglePyramidArea() {
+        // Arrange
         Shape pyramid = new TrianglePyramid(5, 6);
+        // Act
         double area = pyramid.getArea();
+        // Assert
         double expectedArea = Math.sqrt(3) / 4.0 * 5 * 5
                 + 3.0 / 2.0 * 5 * Math.sqrt(6 * 6 - 5 * 5 / 4.0);
-        assert Math.abs(area - expectedArea) < 0.00001;
+        assertEquals(expectedArea, area, DELTA);
     }
 
 
