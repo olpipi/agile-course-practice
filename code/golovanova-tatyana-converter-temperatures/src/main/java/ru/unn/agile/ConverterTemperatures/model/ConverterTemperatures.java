a5a561ec2a0c87aa7859f88de543195f65c20169
Tatyana Golovanova <tgolovanova@avaya.ru>
2018-11-02 00:08:43 +0300

    First iteration

--- code/golovanova-tatyana-converter-temperatures/src/main/java/ru/unn/agile/ConverterTemperatures/model/ConverterTemperatures.java
+++ code/golovanova-tatyana-converter-temperatures/src/main/java/ru/unn/agile/ConverterTemperatures/model/ConverterTemperatures.java
@@ -1,4 +1,5 @@
 package ru.unn.agile.ConverterTemperatures.model;
 
 public interface ConverterTemperatures {
+    public double convert(double tempCelsius);
 }
--- code/golovanova-tatyana-converter-temperatures/src/test/java/ru/unn/agile/ConverterTemperatures/Model/ConverterTemperaturesTest.java
+++ code/golovanova-tatyana-converter-temperatures/src/test/java/ru/unn/agile/ConverterTemperatures/Model/ConverterTemperaturesTest.java
@@ -2,13 +2,22 @@ package ru.unn.agile.ConverterTemperatures.Model;
 
 import org.junit.Test;
 import ru.unn.agile.ConverterTemperatures.model.ConverterTemperatures;
+import ru.unn.agile.ConverterTemperatures.model.ConverterToFahrenheit;
+
+import static org.junit.Assert.assertEquals;
 
 public class ConverterTemperaturesTest {
     private static final double EPSILON = 0.000001;
 
     @Test
-    public void canConvertToFahrenheit(){
-        ConverterTemperatures converter = new ConverterTemperatures();
+    public void canConvertZeroToFahrenheit() {
+        ConverterTemperatures converter = new ConverterToFahrenheit();
+        double tempCelsius = 0.0;
+        double expectedTempFahrenheit = 32.0;
 
+        double calculatingTempFahrenheit = converter.convert(tempCelsius);
+
+        assertEquals(expectedTempFahrenheit, calculatingTempFahrenheit, EPSILON);
     }
+
 }
