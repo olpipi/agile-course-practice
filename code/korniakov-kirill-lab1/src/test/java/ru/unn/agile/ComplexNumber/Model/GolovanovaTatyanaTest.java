ab61fabeb7b45c118b7dc1fe3a951614dca110f5
Tatyana Golovanova <tgolovanova@avaya.ru>
2018-11-01 20:19:39 +0300

    Edited tests

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/GolovanovaTatyanaTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/GolovanovaTatyanaTest.java
@@ -4,16 +4,17 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class GolovanovaTatyanaTest {
+    private final double DELTA = 0.001;
 
     @Test
     public void canAddComplexNumbers() {
         ComplexNumber complexNumber1 = new ComplexNumber(1, 1);
         ComplexNumber complexNumber2 = new ComplexNumber(2, 2);
 
-        ComplexNumber complexNumberForChecking = new ComplexNumber(3, 3);
+        ComplexNumber expectedcomplexNumber = new ComplexNumber(3, 3);
         ComplexNumber addingComplexNumbers = complexNumber1.add(complexNumber2);
 
-        assertEquals(addingComplexNumbers, complexNumberForChecking);
+        assertEquals(expectedcomplexNumber, addingComplexNumbers);
     }
 
     @Test
@@ -21,30 +22,30 @@ public class GolovanovaTatyanaTest {
         ComplexNumber complexNumber1 = new ComplexNumber(1, 1);
         ComplexNumber complexNumber2 = new ComplexNumber(2, 2);
 
-        ComplexNumber complexNumberForChecking = new ComplexNumber(0, 4);
+        ComplexNumber expectedComplexNumber = new ComplexNumber(0, 4);
         ComplexNumber multiplyComplexNumbers = complexNumber1.multiply(complexNumber2);
 
-        assertEquals(multiplyComplexNumbers, complexNumberForChecking);
+        assertEquals(expectedComplexNumber, multiplyComplexNumbers);
     }
 
     @Test
     public void canGetRealOfComplexNumber() {
         ComplexNumber complexNumber = new ComplexNumber(10, 1);
 
-        String checkReal = "10.0";
-        double strReal = complexNumber.getReal();
+        double expectedReal = 10.0;
+        double real = complexNumber.getReal();
 
-        assertEquals(checkReal, checkReal);
+        assertEquals(expectedReal, real, DELTA);
     }
 
     @Test
     public void canToStringComplexNumber() {
         ComplexNumber complexNumber = new ComplexNumber(10, 1);
 
-        String checkStrComplexNumber = "10.0 + 1.0i";
+        String expectedStrComplexNumber = "10.0 + 1.0i";
         String strComplexNumber = complexNumber.toString();
 
-        assertEquals(checkStrComplexNumber, strComplexNumber);
+        assertEquals(expectedStrComplexNumber, strComplexNumber);
     }
 }
 
