49491610721c9de9faba434e524290d9df638075
MariaRumiantseva <manechka96@list.ru>
2018-11-01 16:45:22 +0300

    First settings

---
+++ code/rumiantseva-maria-quadratic-equation-solver/src/test/java/ru/unn/agile/quadraticequation/model/QuadraticEquationTest.java
@@ -0,0 +1,10 @@
+package ru.unn.agile.quadraticequation.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class QuadraticEquationTest {
+    @Test
+    public void firstTest() {
+    }
+}
