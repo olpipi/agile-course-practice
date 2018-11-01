45b441c7f71664522c68c5e406bce65a3a6f0f75
Maria-Oleneva <olenevamaria@yande.ru>
2018-11-01 16:21:58 +0300

    lab1-tdd-line-intersect-plane
    Fix code according to comments.
    Add new Test.

--- code/oleneva-intersect-line-and-plane/src/test/java/ru/unn/agile/intersect/Model/IntersectTest.java
+++ code/oleneva-intersect-line-and-plane/src/test/java/ru/unn/agile/intersect/Model/IntersectTest.java
@@ -8,23 +8,24 @@ import ru.unn.agile.intersect.model.objects.*;
 import static org.junit.Assert.*;
 
 public class IntersectTest {
+    private double delta = 0.0000001;
 
     @Test
     public void canGetPointX() {
         Point pointA = new Point(1, 2, 3);
-        assertEquals(1, pointA.getX(), 0.0000001);
+        assertEquals(1, pointA.getX(), delta);
     }
 
     @Test
     public void canGetPointY() {
         Point pointA = new Point(1, 2, 3);
-        assertEquals(2, pointA.getY(), 0.0000001);
+        assertEquals(2, pointA.getY(), delta);
     }
 
     @Test
     public void canGetPointZ() {
         Point pointA = new Point(1, 2, 3);
-        assertEquals(3, pointA.getZ(), 0.0000001);
+        assertEquals(3, pointA.getZ(), delta);
     }
 
     @Test
@@ -34,9 +35,9 @@ public class IntersectTest {
 
         Line lineL = new Line(pointX, pointY);
 
-        assertEquals(pointX.getX(), lineL.getX().getX(), 0.0000001);
-        assertEquals(pointX.getY(), lineL.getX().getY(), 0.0000001);
-        assertEquals(pointX.getZ(), lineL.getX().getZ(), 0.0000001);
+        assertEquals(pointX.getX(), lineL.getX().getX(), delta);
+        assertEquals(pointX.getY(), lineL.getX().getY(), delta);
+        assertEquals(pointX.getZ(), lineL.getX().getZ(), delta);
     }
 
     @Test
@@ -46,9 +47,9 @@ public class IntersectTest {
 
         Line lineL = new Line(pointX, pointY);
 
-        assertEquals(pointY.getX(), lineL.getY().getX(), 0.0000001);
-        assertEquals(pointY.getY(), lineL.getY().getY(), 0.0000001);
-        assertEquals(pointY.getZ(), lineL.getY().getZ(), 0.0000001);
+        assertEquals(pointY.getX(), lineL.getY().getX(), delta);
+        assertEquals(pointY.getY(), lineL.getY().getY(), delta);
+        assertEquals(pointY.getZ(), lineL.getY().getZ(), delta);
     }
 
     @Test
@@ -59,9 +60,9 @@ public class IntersectTest {
 
         Plane planeP = new Plane(pointA, pointB, pointC);
 
-        assertEquals(pointA.getX(), planeP.getA().getX(), 0.0000001);
-        assertEquals(pointA.getY(), planeP.getA().getY(), 0.0000001);
-        assertEquals(pointA.getZ(), planeP.getA().getZ(), 0.0000001);
+        assertEquals(pointA.getX(), planeP.getA().getX(), delta);
+        assertEquals(pointA.getY(), planeP.getA().getY(), delta);
+        assertEquals(pointA.getZ(), planeP.getA().getZ(), delta);
     }
 
     @Test
@@ -72,9 +73,9 @@ public class IntersectTest {
 
         Plane planeP = new Plane(pointA, pointB, pointC);
 
-        assertEquals(pointB.getX(), planeP.getB().getX(), 0.0000001);
-        assertEquals(pointB.getY(), planeP.getB().getY(), 0.0000001);
-        assertEquals(pointB.getZ(), planeP.getB().getZ(), 0.0000001);
+        assertEquals(pointB.getX(), planeP.getB().getX(), delta);
+        assertEquals(pointB.getY(), planeP.getB().getY(), delta);
+        assertEquals(pointB.getZ(), planeP.getB().getZ(), delta);
     }
 
     @Test
@@ -85,9 +86,9 @@ public class IntersectTest {
 
         Plane planeP = new Plane(pointA, pointB, pointC);
 
-        assertEquals(pointC.getX(), planeP.getC().getX(), 0.0000001);
-        assertEquals(pointC.getY(), planeP.getC().getY(), 0.0000001);
-        assertEquals(pointC.getZ(), planeP.getC().getZ(), 0.0000001);
+        assertEquals(pointC.getX(), planeP.getC().getX(), delta);
+        assertEquals(pointC.getY(), planeP.getC().getY(), delta);
+        assertEquals(pointC.getZ(), planeP.getC().getZ(), delta);
     }
 
     @Test
@@ -97,9 +98,9 @@ public class IntersectTest {
 
         Point vectorV = pointA.createVector(pointB);
 
-        assertEquals(vectorV.getX(), 1, 0.0000001);
-        assertEquals(vectorV.getY(), -2, 0.0000001);
-        assertEquals(vectorV.getZ(), 0, 0.0000001);
+        assertEquals(vectorV.getX(), 1, delta);
+        assertEquals(vectorV.getY(), -2, delta);
+        assertEquals(vectorV.getZ(), 0, delta);
     }
 
     @Test
@@ -109,9 +110,9 @@ public class IntersectTest {
 
         Point vectorProduct = pointA.vectorProduct(pointB);
 
-        assertEquals(vectorProduct.getX(), 2, 0.0000001);
-        assertEquals(vectorProduct.getY(), 1, 0.0000001);
-        assertEquals(vectorProduct.getZ(), -4, 0.0000001);
+        assertEquals(vectorProduct.getX(), 2, delta);
+        assertEquals(vectorProduct.getY(), 1, delta);
+        assertEquals(vectorProduct.getZ(), -4, delta);
 
     }
 
@@ -122,7 +123,7 @@ public class IntersectTest {
 
         double scalarProduct = pointA.scalarProduct(pointB);
 
-        assertEquals(scalarProduct, -3, 0.0000001);
+        assertEquals(scalarProduct, -3, delta);
     }
 
     @Test
@@ -131,9 +132,9 @@ public class IntersectTest {
 
         Point normalPointB = pointB.normalizePoint();
 
-        assertEquals(normalPointB.getX(), 0.6, 0.0000001);
-        assertEquals(normalPointB.getY(), 0.8, 0.0000001);
-        assertEquals(normalPointB.getZ(), 0, 0.0000001);
+        assertEquals(normalPointB.getX(), 0.6, delta);
+        assertEquals(normalPointB.getY(), 0.8, delta);
+        assertEquals(normalPointB.getZ(), 0, delta);
     }
 
     @Test
@@ -146,9 +147,9 @@ public class IntersectTest {
 
         Point normalN = planeP.countNormalToPlane();
 
-        assertEquals(normalN.getX(), -0.707106781188095, 0.0000001);
-        assertEquals(normalN.getY(), 0, 0.0000001);
-        assertEquals(normalN.getZ(), 0.707106781188095, 0.0000001);
+        assertEquals(normalN.getX(), -0.707106781188095, delta);
+        assertEquals(normalN.getY(), 0, delta);
+        assertEquals(normalN.getZ(), 0.707106781188095, delta);
 
     }
 
@@ -167,11 +168,12 @@ public class IntersectTest {
 
         double distance = planeP.countToNormalDistance(lineL);
 
-        assertEquals(distance, -1.0137306107981, 0.0000001);
+        assertEquals(distance, -1.0137306107981, delta);
     }
 
     @Test
     public void canLineIntersectPlaneWithCommonPoint() {
+        //Arrange
         Point pointA = new Point(1, 0, 1);
         Point pointB = new Point(86, 2, 0);
         Point pointC = new Point(0, 1, 0);
@@ -183,13 +185,16 @@ public class IntersectTest {
 
         Plane planeP = new Plane(pointA, pointB, pointC);
 
+        //Assert
         LineIntersectPlane check = new LineIntersectPlane(planeP, lineL);
 
+        //Act
         assertEquals(true, check.checkIntersection());
     }
 
     @Test
     public void canLineIntersectPlaneParallel() {
+        //Arrange
         Point pointA = new Point(1, 94, 0);
         Point pointB = new Point(56, 2, 0);
         Point pointC = new Point(10, 1, 0);
@@ -201,9 +206,32 @@ public class IntersectTest {
 
         Plane planeP = new Plane(pointA, pointB, pointC);
 
+        //Assert
         LineIntersectPlane check = new LineIntersectPlane(planeP, lineL);
 
+        //Act
         assertEquals(false, check.checkIntersection());
+    }
+
+    @Test
+    public void canLineIntersectPlaneWhenLineBelongPlane() {
+        //Arrange
+        Point pointA = new Point(1, 9, 0);
+        Point pointB = new Point(96, 2, 0);
+        Point pointC = new Point(4, 73, 0);
+
+        Point pointX = new Point(1, -87, 0);
+        Point pointY = new Point(2, 2, 0);
+
+        Line lineL = new Line(pointX, pointY);
+
+        Plane planeP = new Plane(pointA, pointB, pointC);
+
+        //Assert
+        LineIntersectPlane check = new LineIntersectPlane(planeP, lineL);
+
+        //Act
+        assertEquals(true, check.checkIntersection());
     }
 
     @Test
