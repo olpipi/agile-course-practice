193faa2d6caccbd60b338f10fdfa68854e510402
MariaRumiantseva <manechka96@list.ru>
2018-11-01 17:36:08 +0300

    Add test for A coef

--- code/rumiantseva-maria-quadratic-equation-solver/src/main/java/ru/unn/agile/quadraticequation/model/QuadraticEquation.java
+++ code/rumiantseva-maria-quadratic-equation-solver/src/main/java/ru/unn/agile/quadraticequation/model/QuadraticEquation.java
@@ -1,4 +1,15 @@
 package ru.unn.agile.quadraticequation.model;
 
 public class QuadraticEquation {
+    public static final double EPSILON = 0.000001;
+
+    private double a;
+
+    public QuadraticEquation(final double a, final double b, final double c) {
+        this.a = a;
+    }
+
+    public double getA() {
+        return a;
+    }
 }
--- code/rumiantseva-maria-quadratic-equation-solver/src/test/java/ru/unn/agile/quadraticequation/model/QuadraticEquationTest.java
+++ code/rumiantseva-maria-quadratic-equation-solver/src/test/java/ru/unn/agile/quadraticequation/model/QuadraticEquationTest.java
@@ -5,6 +5,11 @@ import static org.junit.Assert.*;
 
 public class QuadraticEquationTest {
     @Test
-    public void firstTest() {
+    public void canGetQuadraticCoefficient() {
+        QuadraticEquation func = new QuadraticEquation(5.1, 6.1, 7.1);
+
+        double a = func.getA();
+
+        assertEquals(5.1, a, QuadraticEquation.EPSILON);
     }
 }
