22cf1cb6c3ed61caf79429fbe5e262c168508cf0
Irina20106 <irina20106@list.ru>
2018-11-06 12:22:52 +0300

    KrutoberezhskayaIrinaLab1

---
+++ code/NumbersInWords/NumbersInWords/src/main/java/ru/unn/agile/numbersIn-words/model/NumbersInWords.java
@@ -0,0 +1,88 @@
+package ru.unn.agile.numbersIn-words.model;
+import java.text.DecimalFormat;
+public final class NumbersInWords {
+    private static final String[] tensNames = {
+            ""," Ten"," Twenty"," Thirty"," Forty"," Fifty"," Sixty"," Seventy"," Eighty"," Ninety"
+    };
+    private static final String[] numNames = {
+            ""," One"," Two"," Three"," Four"," Five"," Six"," Seven"," Eight"," Nine"," Ten",
+            " Eleven"," Twelve"," Thirteen"," Fourteen"," Fifteen"," Sixteen"," Seventeen"," Eighteen"," Nineteen"
+    };
+
+    private static final int TEN = 10;
+    private static final int HUNDRED = 100;
+    private static final int ZERO = 0;
+    private static final int THREE = 3;
+    private static final int SIX = 6;
+    private static final int NINE = 9;
+    private static final int TWELVE = 12;
+
+    private NumbersInWords() {
+        /* constructor not needed here */
+    }
+    private static String convertLessThanOneThousand(final int number) {
+        String soFar;
+        int newNumber = number;
+        if (number % HUNDRED < 20){
+            soFar = numNames[number % HUNDRED];
+            newNumber = number / HUNDRED;
+        }
+        else {
+            soFar = numNames[number % TEN];
+            newNumber = number / TEN;
+            soFar = tensNames[newNumber % TEN] + soFar;
+            newNumber = newNumber / TEN;
+        }
+        if (number == 0) return soFar;
+        return numNames[newNumber] + " hundred" + soFar;
+    }
+    public static String convert(long number) {
+        if (number == 0) {
+            return "zero";
+        }
+        String snumber = Long.toString(number);
+        String mask = "000000000000";
+        DecimalFormat df = new DecimalFormat(mask);
+        snumber = df.format(number);
+
+        int billions = Integer.parseInt(snumber.substring(ZERO,THREE));
+        int millions  = Integer.parseInt(snumber.substring(THREE,SIX));
+        int hundredThousands = Integer.parseInt(snumber.substring(SIX,NINE));
+        int thousands = Integer.parseInt(snumber.substring(NINE,TWELVE));
+
+        String tradBillions;
+        switch (billions) {
+            case 0:
+                tradBillions = "";
+                break;
+            default :
+                tradBillions = convertLessThanOneThousand(billions) + " billion ";
+        }
+        String result =  tradBillions;
+
+        String tradMillions;
+        switch (millions) {
+            case 0:
+                tradMillions = "";
+                break;
+            default :
+                tradMillions = convertLessThanOneThousand(millions) + " million ";
+        }
+        result =  result + tradMillions;
+
+        String tradHundredThousands;
+        switch (hundredThousands) {
+            case 0:
+                tradHundredThousands = "";
+                break;
+                default :
+                    tradHundredThousands = convertLessThanOneThousand(hundredThousands) + " thousand ";
+        }
+        result =  result + tradHundredThousands;
+
+        String tradThousand;
+        tradThousand = convertLessThanOneThousand(thousands);
+        result =  result + tradThousand;
+        return result.replaceAll("^\\s+", "").replaceAll("\\b\\s{2,}\\b", " ");
+    }
+}
---
+++ code/NumbersInWords/NumbersInWords/src/test/java/ru/unn/agile/numbersIn-words/model/NumbersInWordsTest.java
@@ -0,0 +1,98 @@
+package ru.unn.agile.numbersIn-words.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NumbersInWordsTest {
+
+  /*
+    @Test
+
+    public void canAddComplexNumbers() {
+        // Arrange
+        ComplexNumber z1 = new ComplexNumber(1, 0);
+        ComplexNumber z2 = new ComplexNumber(0, 1);
+
+        // Act
+        ComplexNumber z = z1.add(z2);
+
+        // Assert
+        assertEquals(new ComplexNumber(1, 1), z);
+    }
+//*/
+    @Test
+    public void canConvertZero() {
+        String convertString = NumbersInWords.convert(0);
+        assertEquals(convertString, "zero");
+    }
+
+    @Test
+    public void canConvertaNumberOfSeveralZeros() {
+        String convertString = NumbersInWords.convert(000);
+        assertEquals(convertString, "zero");
+    }
+
+    @Test
+    public void can convertTheTopTenNumbers() {
+        String convertString = NumbersInWords.convert(6);
+        assertEquals(convertString, "Six");
+    }
+
+    @Test
+    public void can convertMultiplesOfTen() {
+        String convertString = NumbersInWords.convert(80);
+        assertEquals(convertString, "Eighty");
+    }
+
+    @Test
+    public void canConvertNumbersToHundreds() {
+        String convertString = NumbersInWords.convert(54);
+        assertEquals(convertString, "Fifty One");
+    }
+
+    @Test
+    public void can convertMultiplesOfHundred() {
+        String convertString = NumbersInWords.convert(200);
+        assertEquals(convertString, "Two hundred");
+    }
+
+    @Test
+    public void canConvertLessThousand() {
+        String convertString = NumbersInWords.convert(115);
+        assertEquals(convertString, "One hundred Fifteen");
+    }
+
+    @Test
+    public void can convertMultiplesOfThousand() {
+        String convertString = NumbersInWords.convert(3000);
+        assertEquals(convertString, "Three thousand");
+    }
+
+    @Test
+    public void canConvertLessMillion() {
+        String convertString = NumbersInWords.convert(29076);
+        assertEquals(convertString, "Twenty Nine thousand Seventy Six");
+    }
+
+    @Test
+    public void can convertMultiplesOfMillion() {
+        String convertString = NumbersInWords.convert(9000000);
+        assertEquals(convertString, "Nine million");
+    }
+
+    @Test
+    public void canConvertLessBillion() {
+        String convertString = NumbersInWords.convert(7832504);
+        assertEquals(convertString, "Seven million Eight hundred Thirty Two thousand Five hundred Four");
+    }
+
+
+
+
+
+
+
+
+
+}
