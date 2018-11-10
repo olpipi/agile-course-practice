ec2eede8480c5cc09bb27a507cca680491193e1c
inhabitantNewCity <v.andreev.job@gmail.com>
2018-11-11 01:36:47 +0300

    rename javafx controller

--- code/dijkstra/view/src/test/java/ru/unn/agile/dijkstra/view/FakeViewTest.java
+++ code/dijkstra/view/src/test/java/ru/unn/agile/dijkstra/view/FakeViewTest.java
@@ -3,7 +3,6 @@ package ru.unn.agile.dijkstra.view;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class FakeViewTest {
@@ -11,11 +10,5 @@ public class FakeViewTest {
     @Ignore @Test
     public void fakeTest() {
         fail("No tests for view");
-    }
-
-    @Test
-    public void fakeTestView() {
-        DijkstraController viewModel = new DijkstraController();
-        assertEquals(viewModel.getClass(), viewModel.getClass());
     }
 }
