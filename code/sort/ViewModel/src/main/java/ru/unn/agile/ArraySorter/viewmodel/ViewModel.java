e2c2bf4ee938508990534cdfece9d5de6b96cc1a
Podolov <kirill-podolov@yandex.ru>
2018-11-11 22:38:25 +0300

    Added some tests to add elements to Array

--- code/sort/ViewModel/src/main/java/ru/unn/agile/ArraySorter/viewmodel/ViewModel.java
+++ code/sort/ViewModel/src/main/java/ru/unn/agile/ArraySorter/viewmodel/ViewModel.java
@@ -1,15 +1,41 @@
 package ru.unn.agile.ArraySorter.viewmodel;
 
+import java.util.ArrayList;
+
 public class ViewModel {
     private String newElem;
+    private String outputArray;
 
+    ArrayList<Double> Array = new ArrayList<Double>();
 
     public ViewModel() {
         newElem = "";
+        outputArray = "";
+
     }
 
+    public String getOutputArray()
+    {
+        return outputArray;
+    }
 
     public String getNewElem() {
         return newElem;
+    }
+
+    public void setNewElem(final String newElem){
+        if (newElem.equals(this.newElem)) {
+            return;
+        }
+
+        this.newElem = newElem;
+    }
+
+    public void Add()
+    {
+        double value = Double.parseDouble(newElem);
+        Array.add(value);
+
+        outputArray = Array.toString();
     }
 }
--- code/sort/ViewModel/src/test/java/ru/unn/agile/ArraySorter/viewmodel/ViewModelTests.java
+++ code/sort/ViewModel/src/test/java/ru/unn/agile/ArraySorter/viewmodel/ViewModelTests.java
@@ -23,4 +23,28 @@ public class ViewModelTests {
     public void canSetDefaultValues() {
         assertEquals("", viewModel.getNewElem());
     }
+
+    @Test
+    public void canAddOneElementToArray() {
+        viewModel.setNewElem("3");
+        viewModel.Add();
+
+        assertEquals("[3.0]", viewModel.getOutputArray());
+    }
+
+    @Test
+    public void canAddSeveralElementsToArray() {
+        viewModel.setNewElem("6");
+        viewModel.Add();
+        viewModel.setNewElem("4");
+        viewModel.Add();
+        viewModel.setNewElem("1");
+        viewModel.Add();
+        viewModel.setNewElem("10");
+        viewModel.Add();
+        viewModel.setNewElem("8");
+        viewModel.Add();
+
+        assertEquals("[6.0, 4.0, 1.0, 10.0, 8.0]", viewModel.getOutputArray());
+    }
 }
