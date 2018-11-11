ba006e7b8f72a19c6a8ae4f5eca11e287f6f07d5
Podolov <kirill-podolov@yandex.ru>
2018-11-11 21:10:25 +0300

    Added first test

--- code/sort/ViewModel/src/main/java/ru/unn/agile/ArraySorter/viewmodel/ViewModel.java
+++ code/sort/ViewModel/src/main/java/ru/unn/agile/ArraySorter/viewmodel/ViewModel.java
@@ -1,4 +1,15 @@
 package ru.unn.agile.ArraySorter.viewmodel;
 
 public class ViewModel {
+    private String newElem;
+
+
+    public ViewModel() {
+        newElem = "";
+    }
+
+
+    public String getNewElem() {
+        return newElem;
+    }
 }
--- code/sort/ViewModel/src/test/java/ru/unn/agile/ArraySorter/viewmodel/ViewModelTests.java
+++ code/sort/ViewModel/src/test/java/ru/unn/agile/ArraySorter/viewmodel/ViewModelTests.java
@@ -1,4 +1,26 @@
 package ru.unn.agile.ArraySorter.viewmodel;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ViewModelTests {
+    private ViewModel viewModel;
+
+    @Before
+    public void setUp() {
+        viewModel = new ViewModel();
+    }
+
+    @After
+    public void tearDown() {
+        viewModel = null;
+    }
+
+    @Test
+    public void canSetDefaultValues() {
+        assertEquals("", viewModel.getNewElem());
+    }
 }
