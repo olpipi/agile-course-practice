18e16b7670bd3150fcaac4e5e26dc220489293a8
alexanderflegontov <manshipnn@gmail.com>
2018-11-11 20:57:36 +0300

    Added the first implementation

--- code/queue/ViewModel/src/main/java/ru/unn/agile/queue/viewmodel/ViewModel.java
+++ code/queue/ViewModel/src/main/java/ru/unn/agile/queue/viewmodel/ViewModel.java
@@ -1,4 +1,14 @@
 package ru.unn.agile.queue.viewmodel;
 
 public class ViewModel {
+    private String newElem;
+
+    public ViewModel() {
+        newElem = "";
+    }
+
+    public String getNewElem() {
+        return newElem;
+    }
+
 }
