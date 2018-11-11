d06900f6b404840486b5afd46d8fc5ef94c37f75
alexanderflegontov <manshipnn@gmail.com>
2018-11-11 22:38:09 +0300

    Added tests related to adding to queue

--- code/queue/ViewModel/src/main/java/ru/unn/agile/queue/viewmodel/ViewModel.java
+++ code/queue/ViewModel/src/main/java/ru/unn/agile/queue/viewmodel/ViewModel.java
@@ -1,14 +1,23 @@
 package ru.unn.agile.queue.viewmodel;
 
+import ru.unn.agile.queue.model.*;
+
 public class ViewModel {
     private String newElem;
+    private String outputQueue;
+
+    private Queue<Double> queue;
 
     public ViewModel() {
+        queue = new Queue<Double>();
         newElem = "";
+        outputQueue = "";
     }
+
 
     public String getNewElem() {
         return newElem;
     }
+
 
 }
--- code/queue/ViewModel/src/test/java/ru/unn/agile/queue/viewmodel/ViewModelTests.java
+++ code/queue/ViewModel/src/test/java/ru/unn/agile/queue/viewmodel/ViewModelTests.java
@@ -1,4 +1,50 @@
 package ru.unn.agile.queue.viewmodel;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class ViewModelTests {
+    private ViewModel viewModel;
+
+    @Before
+    public void setUp() {
+        viewModel = new ViewModel();
+    }
+
+    @After
+    public void tearDown() {
+        viewModel = null;
+    }
+
+    @Test
+    public void canSetDefaultValues() {
+        assertEquals("", viewModel.getNewElem());
+    }
+
+    @Test
+    public void canAddOneElemToQueue() {
+        viewModel.setNewElem("5.43");
+        viewModel.Add();
+
+        assertEquals("[5.43]", viewModel.getOutputQueue());
+    }
+
+
+    @Test
+    public void canAddQueue() {
+        viewModel.setNewElem("1.0");
+        viewModel.Add();
+        viewModel.setNewElem("3.0");
+        viewModel.Add();
+        viewModel.setNewElem("4.0");
+        viewModel.Add();
+        viewModel.setNewElem("8.0");
+        viewModel.Add();
+
+        assertEquals("[1.0, 3.0, 4.0, 8.0]", viewModel.getOutputQueue());
+    }
+
 }
