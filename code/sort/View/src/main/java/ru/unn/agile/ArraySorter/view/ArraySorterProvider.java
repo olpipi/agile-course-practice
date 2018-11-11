dcaead97f770bcfc215bd22e91ad546643907f16
Podolov <kirill-podolov@yandex.ru>
2018-11-11 16:40:48 +0300

    Fixed some presentation layer

--- code/sort/View/src/main/java/ru/unn/agile/ArraySorter/view/ArraySorterProvider.java
+++ code/sort/View/src/main/java/ru/unn/agile/ArraySorter/view/ArraySorterProvider.java
@@ -2,16 +2,20 @@ package ru.unn.agile.ArraySorter.view;
 
 import javax.swing.*;
 
-public class ArraySorterProvider {
-    private JPanel panel1;
-    private JTextField textField1;
+public final class ArraySorterProvider {
+    private JPanel mainPanel;
+    private JTextField addElementField;
     private JButton addElementButton;
-    private JButton clearButton;
+    private JButton clearArrayButton;
     private JButton sortArrayButton;
+    private JLabel sourceArray;
+    private JLabel array;
+    private JLabel sortedArray;
+    private JLabel resultSortedArray;
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         JFrame frame = new JFrame("ArraySorterProvider");
-        frame.setContentPane(new ArraySorterProvider().panel1);
+        frame.setContentPane(new ArraySorterProvider().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
