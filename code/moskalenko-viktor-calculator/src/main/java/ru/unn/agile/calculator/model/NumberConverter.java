75f2dca937d1b0b3f94a5420ccfca4ebe84f6b10
Moskalenko Viktor <moskalenkoviktor@list.ru>
2018-11-14 20:47:47 +0300

    Changed map to enum

--- code/moskalenko-viktor-calculator/src/main/java/ru/unn/agile/calculator/model/NumberConverter.java
+++ code/moskalenko-viktor-calculator/src/main/java/ru/unn/agile/calculator/model/NumberConverter.java
@@ -6,28 +6,36 @@ public final class NumberConverter {
 
     private static final String UNKNOWN_NUMBER_LENGTH = "Unknown number length";
     private static final String UNKNOWN_NUMBER_SYSTEM = "Unknown number system";
-    private static final Map<Character, Integer> LETTER_TO_SYSTEM;
     private static final int BINARY_SYSTEM = 2;
     private static final int OCTAL_SYSTEM = 8;
     private static final int HEXADECIMAL_SYSTEM = 16;
 
-    static {
-        Map<Character, Integer> map = new TreeMap<>();
-        map.put('b', BINARY_SYSTEM);
-        map.put('o', OCTAL_SYSTEM);
-        map.put('x', HEXADECIMAL_SYSTEM);
-        LETTER_TO_SYSTEM = Collections.unmodifiableMap(map);
+    private enum LetterToSystem {
+        b(BINARY_SYSTEM), o(OCTAL_SYSTEM), x(HEXADECIMAL_SYSTEM);
+
+        private final int value;
+        LetterToSystem(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
     }
 
     public static int parse(final String number) {
         if (number.length() < 2) {
             throw new IllegalArgumentException(UNKNOWN_NUMBER_LENGTH);
         }
-        final char letterSystem = number.charAt(0);
-        if (!LETTER_TO_SYSTEM.containsKey(letterSystem)) {
+        final String letterSystem = number.substring(0, 1);
+
+        int system;
+        try {
+            system = LetterToSystem.valueOf(letterSystem).getValue();
+        } catch (Exception ex) {
             throw new IllegalArgumentException(UNKNOWN_NUMBER_SYSTEM);
         }
-        return Integer.parseInt(number.substring(1), LETTER_TO_SYSTEM.get(letterSystem));
+        return Integer.parseInt(number.substring(1), system);
     }
 
     public static Integer tryParse(final String number) {
