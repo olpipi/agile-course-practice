0035c9af2734deddfda89420b6b9018d436f064a
Ivan Magazinnik <magazinnik.ivan@gmail.com>
2018-11-17 16:35:03 +0300

    add incorrect value report implementation

--- code/vector-distance/ViewModel/src/main/java/ru/unn/agile/VectorDistance/viewmodel/ViewModel.java
+++ code/vector-distance/ViewModel/src/main/java/ru/unn/agile/VectorDistance/viewmodel/ViewModel.java
@@ -2,6 +2,8 @@ package ru.unn.agile.VectorDistance.viewmodel;
 
 import javafx.beans.binding.BooleanBinding;
 import javafx.beans.property.*;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -15,6 +17,7 @@ import java.util.List;
 
 public class ViewModel {
     private static final String DEFAULT_DELIMITER = " ";
+    private static final String VALID_VECTOR_PATTERN = "^(-?[0-9]\\s?)+$";
     private final StringProperty vectorX = new SimpleStringProperty();
     private final StringProperty vectorY = new SimpleStringProperty();
     private final StringProperty result = new SimpleStringProperty();
@@ -28,6 +31,8 @@ public class ViewModel {
     }
 
     private final ObjectProperty<Distance> distance = new SimpleObjectProperty<>();
+
+    private final List<ValueChangeListener> valueChangedListeners = new ArrayList<>();
 
     public ViewModel() {
         vectorX.set("");
@@ -45,33 +50,23 @@ public class ViewModel {
             }
         };
         calculationDisabled.bind(couldCalculate.not());
-    }
+        // Add listeners to the input text fields
+        final List<StringProperty> fields = new ArrayList<StringProperty>() { {
+            add(vectorX);
+            add(vectorY);
+        } };
 
-    private static List<String> getNumbersArrayFromString(final String numbersString) {
-        return Arrays.asList(numbersString.split(DEFAULT_DELIMITER));
-    }
-
-    private float[] getFloatArrayFromStringList(final List<String> stringList) {
-        float[] floatArray = new float[stringList.size()];
-
-        int i = 0;
-
-        for (String number : stringList) {
-            floatArray[i++] = Float.parseFloat(number);
+        for (StringProperty field : fields) {
+            final ValueChangeListener listener = new ValueChangeListener();
+            field.addListener(listener);
+            valueChangedListeners.add(listener);
         }
-        return floatArray;
-    }
-
-    private FloatVector getVectorFromInputString(final String inputString) {
-        List<String> singleNumbers = getNumbersArrayFromString(inputString);
-        return new FloatVector(getFloatArrayFromStringList(singleNumbers));
     }
 
     public void calculate() {
 
         FloatVector a = getVectorFromInputString(vectorX.get());
         FloatVector b = getVectorFromInputString(vectorY.get());
-
         result.set(String.valueOf(distance.get().apply(a, b)));
         status.set(Status.SUCCESS.toString());
     }
@@ -106,13 +101,49 @@ public class ViewModel {
         return calculationDisabled.get();
     }
 
+
+    private static List<String> getNumbersArrayFromString(final String numbersString) {
+        return Arrays.asList(numbersString.split(DEFAULT_DELIMITER));
+    }
+
+    private float[] getFloatArrayFromStringList(final List<String> stringList) {
+        float[] floatArray = new float[stringList.size()];
+
+        int i = 0;
+
+        for (String number : stringList) {
+            floatArray[i++] = Float.parseFloat(number);
+        }
+        return floatArray;
+    }
+
+    private FloatVector getVectorFromInputString(final String inputString) {
+        List<String> singleNumbers = getNumbersArrayFromString(inputString);
+        return new FloatVector(getFloatArrayFromStringList(singleNumbers));
+    }
+
+    private boolean checkInputIsValid(final String vectorInput) {
+        return vectorInput.matches(VALID_VECTOR_PATTERN);
+    }
+
     private Status getInputStatus() {
         Status inputStatus = Status.READY;
         if (vectorX.get().isEmpty() || vectorY.get().isEmpty()) {
             inputStatus = Status.WAITING;
         }
+        if (!checkInputIsValid(vectorX.get()) || !checkInputIsValid(vectorY.get())) {
+            inputStatus = Status.BAD_FORMAT;
+        }
 
         return inputStatus;
+    }
+
+    private class ValueChangeListener implements ChangeListener<String> {
+        @Override
+        public void changed(final ObservableValue<? extends String> observable,
+                            final String oldValue, final String newValue) {
+            status.set(getInputStatus().toString());
+        }
     }
 }
 
