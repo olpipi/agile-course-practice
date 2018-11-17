0bd8cabc303b283fafd272ba08cbf48bf7ff5278
Ivan Magazinnik <magazinnik.ivan@gmail.com>
2018-11-17 14:47:19 +0300

    Create app view design

--- code/vector-distance/View/src/main/java/ru/unn/agile/VectorDistance/view/VectorDistance.java
+++ code/vector-distance/View/src/main/java/ru/unn/agile/VectorDistance/view/VectorDistance.java
@@ -1,6 +1,9 @@
 package ru.unn.agile.VectorDistance.view;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import ru.unn.agile.VectorDistance.model.VectorDistance.Distance;
 import ru.unn.agile.VectorDistance.viewmodel.ViewModel;
 
@@ -9,6 +12,16 @@ public class VectorDistance {
     private ViewModel viewModel;
     @FXML
     private ComboBox<Distance> cbDistances;
+    @FXML
+    private TextField txtVectorX;
+    @FXML
+    private TextField txtVectorY;
+    @FXML
+    private Button btnCalc;
+    @FXML
+    private Label lbResult;
+    @FXML
+    private Label lbStatus;
     @FXML
     void initialize() {
         cbDistances.valueProperty().bindBidirectional(viewModel.distanceProperty());
