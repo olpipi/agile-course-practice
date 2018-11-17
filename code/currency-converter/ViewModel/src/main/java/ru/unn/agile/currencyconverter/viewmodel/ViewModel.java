c26132153f7fd113e7e7345d65c795048a8e0f35
Alexander Rumyantsev <cetygamer@gmail.com>
2018-11-17 22:07:52 +0300

    created string properties for all fields

--- code/currency-converter/ViewModel/src/main/java/ru/unn/agile/currencyconverter/viewmodel/ViewModel.java
+++ code/currency-converter/ViewModel/src/main/java/ru/unn/agile/currencyconverter/viewmodel/ViewModel.java
@@ -1,4 +1,65 @@
 package ru.unn.agile.currencyconverter.viewmodel;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import ru.unn.agile.currencyconverter.model.CurrencyConverter;
+
 public class ViewModel {
+    private StringProperty addSrcCodeStr = new SimpleStringProperty();
+    private StringProperty addTgtCodeStr = new SimpleStringProperty();
+    private StringProperty addRateStr = new SimpleStringProperty();
+
+    private StringProperty convSrcCodeStr = new SimpleStringProperty();
+    private StringProperty convTgtCodeStr = new SimpleStringProperty();
+    private StringProperty convAmountStr = new SimpleStringProperty();
+
+    private StringProperty currPairsSrt = new SimpleStringProperty();
+    private StringProperty resultSrt = new SimpleStringProperty();
+
+    public StringProperty getAddSrcCodeStr() {
+        return addSrcCodeStr;
+    }
+
+    public StringProperty getAddTgtCodeStr() {
+        return addTgtCodeStr;
+    }
+
+    public StringProperty getAddRateStr() {
+        return addRateStr;
+    }
+
+    public StringProperty getConvSrcCodeStr() {
+        return convSrcCodeStr;
+    }
+
+    public StringProperty getConvTgtCodeStr() {
+        return convTgtCodeStr;
+    }
+
+    public StringProperty getConvAmountStr() {
+        return convAmountStr;
+    }
+
+    public StringProperty getCurrPairsSrt() {
+        return currPairsSrt;
+    }
+
+    public StringProperty getResultSrt() {
+        return resultSrt;
+    }
+
+    public ViewModel() {
+        initDefaultFields();
+    }
+
+    private void initDefaultFields() {
+        addSrcCodeStr.set("");
+        addTgtCodeStr.set("");
+        addRateStr.set("");
+        convSrcCodeStr.set("");
+        convTgtCodeStr.set("");
+        convAmountStr.set("");
+        currPairsSrt.set("");
+        resultSrt.set("");
+    }
 }
--- code/currency-converter/ViewModel/src/test/java/ru/unn/agile/currencyconverter/viewmodel/ViewModelTest.java
+++ code/currency-converter/ViewModel/src/test/java/ru/unn/agile/currencyconverter/viewmodel/ViewModelTest.java
@@ -1,4 +1,21 @@
 package ru.unn.agile.currencyconverter.viewmodel;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ViewModelTest {
+    @Test
+    public void initViewModelWithDefaultFields() {
+        ViewModel viewModel = new ViewModel();
+
+        assertEquals("", viewModel.getAddSrcCodeStr().get());
+        assertEquals("", viewModel.getAddTgtCodeStr().get());
+        assertEquals("", viewModel.getAddRateStr().get());
+        assertEquals("", viewModel.getConvSrcCodeStr().get());
+        assertEquals("", viewModel.getConvTgtCodeStr().get());
+        assertEquals("", viewModel.getConvAmountStr().get());
+        assertEquals("", viewModel.getCurrPairsSrt().get());
+        assertEquals("", viewModel.getResultSrt().get());
+    }
 }
