150b74b1ae3f6f466ed36f8cc278462abfd95330
Ekaterina Shiryaeva <shiryaevae01@gmail.com>
2018-11-18 10:34:18 +0300

    Added initialization

--- code/polynomial-calculator/src/main/java/ru/unn/agile/polynomial/model/Polynomial.java
+++ code/polynomial-calculator/src/main/java/ru/unn/agile/polynomial/model/Polynomial.java
@@ -1,5 +1,47 @@
 package ru.unn.agile.polynomial.model;
 
 public class Polynomial {
+    public static final double DELTA = 0.000001;
+
+    private double[] coeffs;
+    private int degree;
+
+    public Polynomial() {
+        this.coeffs = null;
+        this.degree = -1;
+    }
+
+    public Polynomial(final double[] coef) {
+        int firstElem = 0;
+        while ((firstElem < coef.length) && (coef[firstElem] == 0)) {
+            firstElem++;
+        }
+        if (firstElem == coef.length) {
+            this.coeffs = null;
+            this.degree = -1;
+            return;
+        }
+
+        int length = coef.length - firstElem;
+        this.coeffs = new double[length];
+        for (int i = length; i > 0; i--) {
+            this.coeffs[length - i] = coef[firstElem + (length - i)];
+        }
+
+        this.degree = length - 1;
+    }
+
+    public Polynomial(final Polynomial p) {
+        degree = p.degree;
+        coeffs = p.coeffs;
+    }
+
+    public double[] getCoeffs() {
+        return this.coeffs;
+    }
+
+    public int getDegree() {
+        return this.degree;
+    }
 }
 
--- code/polynomial-calculator/src/test/java/ru/unn/agile/polynomial/model/PolynomialTest.java
+++ code/polynomial-calculator/src/test/java/ru/unn/agile/polynomial/model/PolynomialTest.java
@@ -5,6 +5,44 @@ import static org.junit.Assert.*;
 
 public class PolynomialTest {
     @Test
-    public void test() {
+    public void checkInit() {
+        double[] result = {1.0, -2.0, 3.0, -4.0};
+
+        Polynomial p = new Polynomial(result);
+
+        assertArrayEquals(result, p.getCoeffs(), Polynomial.DELTA);
+        assertEquals(3, p.getDegree());
+    }
+
+    @Test
+    public void checkInitWithFirstZero() {
+        double[] toInit = {0.0, 0.0, 3.0};
+        double[] result = {3.0};
+
+        Polynomial p = new Polynomial(toInit);
+
+        assertArrayEquals(result, p.getCoeffs(), Polynomial.DELTA);
+        assertEquals(0, p.getDegree());
+    }
+
+    @Test
+    public void checkInitWithZero() {
+        double[] toInit = {1.0, 0.0, 3.0};
+        double[] result = {1.0, 0.0, 3.0};
+
+        Polynomial p = new Polynomial(toInit);
+
+        assertArrayEquals(result, p.getCoeffs(), Polynomial.DELTA);
+        assertEquals(2, p.getDegree());
+    }
+
+    @Test
+    public void checkInitWithOnlyZero() {
+        double[] toInit = {0.0};
+
+        Polynomial p = new Polynomial(toInit);
+
+        assertArrayEquals(null, p.getCoeffs(), Polynomial.DELTA);
+        assertEquals(-1, p.getDegree());
     }
 }
