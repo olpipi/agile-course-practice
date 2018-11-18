10f5c9774ed51879aecdc18247ab611e73d7ca26
Ekaterina Shiryaeva <shiryaevae01@gmail.com>
2018-11-18 10:33:01 +0300

    Added project to gradle settings

---
+++ code/polynomial-calculator/src/main/java/ru/unn/agile/polynomial/model/Polynomial.java
@@ -0,0 +1,5 @@
+package ru.unn.agile.polynomial.model;
+
+public class Polynomial {
+}
+
---
+++ code/polynomial-calculator/src/test/java/ru/unn/agile/polynomial/model/PolynomialTest.java
@@ -0,0 +1,10 @@
+package ru.unn.agile.polynomial.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PolynomialTest {
+    @Test
+    public void test() {
+    }
+}
