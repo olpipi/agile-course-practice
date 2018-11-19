c78aa315442983155f089c948a67a3e9ce699450
Andrey Kochuev <kochuevai@gmail.com>
2018-11-19 18:23:07 +0300

    refactoring

--- code/polynomial-calculator/ViewModel/src/main/java/ru/unn/agile/polynomial/viewmodel/ViewModel.java
+++ code/polynomial-calculator/ViewModel/src/main/java/ru/unn/agile/polynomial/viewmodel/ViewModel.java
@@ -10,9 +10,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ViewModel {
-    private StringProperty firstPolynomialStr = new SimpleStringProperty();
-    private StringProperty secondPolynomialStr = new SimpleStringProperty();
-    private StringProperty resultStr = new SimpleStringProperty();
+    private final StringProperty firstPolynomialStr = new SimpleStringProperty();
+    private final StringProperty secondPolynomialStr = new SimpleStringProperty();
+    private final StringProperty resultStr = new SimpleStringProperty();
+
+    private final int GROUP_MATCH_1 = 1;// [-]?\d\.*\d*)x (-3)x^3
+    private final int GROUP_MATCH_2 = 2;// x\^(\d\.*\d*) -3x^(3)
+    private final int GROUP_MATCH_3 = 3;// ([-]?\d+\.*\d*)x (-5)x
+    private final int GROUP_MATCH_4 = 4;// ([-]?\d+\.*\d*) (-42.0)
 
     public StringProperty firstPolynomialStrProperty() {
         return firstPolynomialStr;
@@ -46,27 +51,25 @@ public class ViewModel {
         initDefaultFields();
     }
 
-    public Polynomial parsePolynomial(String polynomialStr) {
+    public Polynomial parsePolynomial(final String polynomialStr) {
         Pattern pattern =
                 Pattern.compile("([-]?\\d\\.*\\d*)x\\^(\\d\\.*\\d*)|([-]?\\d+\\.*\\d*)x|([-]?\\d+\\.*\\d*)");
         Matcher matcher = pattern.matcher(polynomialStr);
         ArrayList<Pair<Integer, Double>> array = new ArrayList<>();
         while (matcher.find()) {
-            if (matcher.group(1) != null && matcher.group(2) != null) {
-                array.add(new Pair<>(Integer.valueOf(matcher.group(2))
-                        , Double.valueOf(matcher.group(1))));
-            }
-            else if (matcher.group(3) != null) {
-                array.add(new Pair<>(1, Double.valueOf(matcher.group(3))));
-            }
-            else if (matcher.group(4) != null) {
-                array.add(new Pair<>(0, Double.valueOf(matcher.group(4))));
+            if (matcher.group(GROUP_MATCH_1) != null && matcher.group(GROUP_MATCH_2) != null) {
+                array.add(new Pair<>(Integer.valueOf(matcher.group(GROUP_MATCH_2)),
+                        Double.valueOf(matcher.group(GROUP_MATCH_1))));
+            } else if (matcher.group(GROUP_MATCH_3) != null) {
+                array.add(new Pair<>(1, Double.valueOf(matcher.group(GROUP_MATCH_3))));
+            } else if (matcher.group(GROUP_MATCH_4) != null) {
+                array.add(new Pair<>(0, Double.valueOf(matcher.group(GROUP_MATCH_4))));
             }
         }
         double[] coeffs = new double[array.get(0).getKey() + 1];
         for (int i = 0; i < array.size(); i++) {
-            coeffs[coeffs.length - 1 
-                    - array.get(i).getKey().intValue()] = array.get(i).getValue().doubleValue();
+            coeffs[coeffs.length - 1 - array.get(i).getKey().intValue()]
+                    = array.get(i).getValue().doubleValue();
         }
         return new Polynomial(coeffs);
     }
