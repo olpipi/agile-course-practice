4ab2199b84520d2e098a9d2487689553cbd13cc1
olgaIvanova <olka-lyapa@mail.ru>
2018-11-19 23:52:00 +0300

    created string properties for all fields

--- code/triangle/ViewModel/src/main/java/ru/unn/agile/triangle/viewmodel/ViewModel.java
+++ code/triangle/ViewModel/src/main/java/ru/unn/agile/triangle/viewmodel/ViewModel.java
@@ -1,4 +1,38 @@
 package ru.unn.agile.triangle.viewmodel;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class ViewModel {
+    private StringProperty aStr = new SimpleStringProperty();
+    private StringProperty bStr = new SimpleStringProperty();
+    private StringProperty cStr = new SimpleStringProperty();
+    private StringProperty resultSrt = new SimpleStringProperty();
+
+    public StringProperty getAStr() {
+        return aStr;
+    }
+
+    public StringProperty getBStr() {
+        return bStr;
+    }
+
+    public StringProperty getCStr() {
+        return cStr;
+    }
+
+    public StringProperty getResultSrt() {
+        return resultSrt;
+    }
+
+    public ViewModel() {
+        initDefaultFields();
+    }
+
+    private void initDefaultFields() {
+        aStr.set("");
+        bStr.set("");
+        cStr.set("");
+        resultSrt.set("");
+    }
 }
--- code/triangle/ViewModel/src/test/java/ru/unn/agile/triangle/viewmodel/ViewModelTest.java
+++ code/triangle/ViewModel/src/test/java/ru/unn/agile/triangle/viewmodel/ViewModelTest.java
@@ -1,4 +1,17 @@
 package ru.unn.agile.triangle.viewmodel;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ViewModelTest {
+    @Test
+    public void initViewModelWithDefaultFields() {
+        ViewModel viewModel = new ViewModel();
+
+        assertEquals("", viewModel.getAStr().get());
+        assertEquals("", viewModel.getBStr().get());
+        assertEquals("", viewModel.getCStr().get());
+        assertEquals("", viewModel.getResultSrt().get());
+    }
 }
