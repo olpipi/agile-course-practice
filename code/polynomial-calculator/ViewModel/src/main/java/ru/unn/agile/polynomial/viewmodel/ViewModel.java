4edfdf5524e637a955f03f048e3b6230edb2cf38
Andrey Kochuev <kochuevai@gmail.com>
2018-11-19 15:16:12 +0300

    created string properties

--- code/polynomial-calculator/ViewModel/src/main/java/ru/unn/agile/polynomial/viewmodel/ViewModel.java
+++ code/polynomial-calculator/ViewModel/src/main/java/ru/unn/agile/polynomial/viewmodel/ViewModel.java
@@ -1,4 +1,33 @@
 package ru.unn.agile.polynomial.viewmodel;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import ru.unn.agile.polynomial.model.Polynomial;
+
 public class ViewModel {
+    private StringProperty firstPolynomialStr = new SimpleStringProperty();
+    private StringProperty secondPolynomialStr = new SimpleStringProperty();
+    private StringProperty resultSrt = new SimpleStringProperty();
+
+    public StringProperty getFirstPolynomialStr() {
+        return firstPolynomialStr;
+    }
+
+    public StringProperty getSecondPolynomialStr() {
+        return secondPolynomialStr;
+    }
+
+    public StringProperty getResultSrt() {
+        return resultSrt;
+    }
+
+    public ViewModel() {
+        initDefaultFields();
+    }
+
+    private void initDefaultFields() {
+        firstPolynomialStr.set("");
+        secondPolynomialStr.set("");
+        resultSrt.set("");
+    }
 }
--- code/polynomial-calculator/ViewModel/src/test/java/ru/unn/agile/polynomial/viewmodel/ViewModelTest.java
+++ code/polynomial-calculator/ViewModel/src/test/java/ru/unn/agile/polynomial/viewmodel/ViewModelTest.java
@@ -1,4 +1,14 @@
 package ru.unn.agile.polynomial.viewmodel;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class ViewModelTest {
+    @Test
+    public void createViewModelWithDefaultFields() {
+        ViewModel viewModel = new ViewModel();
+        assertEquals("", viewModel.getFirstPolynomialStr().get());
+        assertEquals("", viewModel.getSecondPolynomialStr().get());
+        assertEquals("", viewModel.getResultSrt().get());
+    }
 }
