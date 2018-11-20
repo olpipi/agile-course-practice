94bf58aecc67d8471efcea1ad3e3dfe696af9efc
KryukovaPolina <paulina-17@yandex.ru>
2018-11-20 23:32:49 +0300

    basic tests implementation

--- code/binary-search/ViewModel/src/main/java/ru/unn/agile/binarysearch/viewmodel/ViewModel.java
+++ code/binary-search/ViewModel/src/main/java/ru/unn/agile/binarysearch/viewmodel/ViewModel.java
@@ -1,16 +1,38 @@
 package ru.unn.agile.binarysearch.viewmodel;
 
-import javafx.beans.binding.BooleanBinding;
-import javafx.beans.property.*;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import ru.unn.agile.binarysearch.model.BinarySearch;
+//import javafx.beans.binding.BooleanBinding;
+//import javafx.beans.property.*;
+//import javafx.beans.value.ChangeListener;
+//import javafx.beans.value.ObservableValue;
+//import javafx.collections.FXCollections;
+//import javafx.collections.ObservableList;
+//import ru.unn.agile.binarysearch.model.BinarySearch;
 
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 
 public class ViewModel {
+
+    private String arrayTextBoxValue;
+    private String elementTextBoxValue;
+    private String status;
+
+    public ViewModel() {
+        arrayTextBoxValue = "";
+        elementTextBoxValue = "";
+        status = "";
+    }
+
+    public String getArrayTextBoxValue() {
+        return arrayTextBoxValue;
+    }
+
+    public String getElementTextBoxValue() {
+        return elementTextBoxValue;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 
 }
--- code/binary-search/ViewModel/src/test/java/ru/unn/agile/binarysearch/viewmodel/ViewModelTests.java
+++ code/binary-search/ViewModel/src/test/java/ru/unn/agile/binarysearch/viewmodel/ViewModelTests.java
@@ -1,9 +1,9 @@
 package ru.unn.agile.binarysearch.viewmodel;
 
-import org.junit.After;
-import org.junit.Before;
+//import org.junit.After;
+//import org.junit.Before;
 import org.junit.Test;
-import ru.unn.agile.binarysearch.model.BinarySearch;
+//import ru.unn.agile.binarysearch.model.BinarySearch;
 
 import static org.junit.Assert.*;
 
