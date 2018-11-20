5e8657ec42f0293c9c0a76622062239c840f29b0
KryukovaPolina <paulina-17@yandex.ru>
2018-11-21 00:25:55 +0300

    simple set methods

--- code/binary-search/ViewModel/src/main/java/ru/unn/agile/binarysearch/viewmodel/ViewModel.java
+++ code/binary-search/ViewModel/src/main/java/ru/unn/agile/binarysearch/viewmodel/ViewModel.java
@@ -6,7 +6,7 @@ package ru.unn.agile.binarysearch.viewmodel;
 //import javafx.beans.value.ObservableValue;
 //import javafx.collections.FXCollections;
 //import javafx.collections.ObservableList;
-//import ru.unn.agile.binarysearch.model.BinarySearch;
+import ru.unn.agile.binarysearch.model.BinarySearch;
 
 //import java.util.ArrayList;
 //import java.util.List;
@@ -16,11 +16,13 @@ public class ViewModel {
     private String arrayTextBoxValue;
     private String elementTextBoxValue;
     private String status;
+    private BinarySearch search;
 
     public ViewModel() {
         arrayTextBoxValue = "";
         elementTextBoxValue = "";
         status = "";
+        search = null;
     }
 
     public String getArrayTextBoxValue() {
@@ -33,6 +35,23 @@ public class ViewModel {
 
     public String getStatus() {
         return status;
+    }
+
+    public void setArrayTextBoxValue(final String input) {
+        String[] split = input.split(",");
+        int[] arr = new int[split.length];
+        for (int i = 0; i < split.length; i++) {
+            arr[i] = Integer.parseInt(split[i]);
+        }
+        search = new BinarySearch(arr);
+    }
+
+    public int[] getBinarySearchArray() {
+        return search.getArray();
+    }
+
+    public void setElementTextBoxValue(final String input) {
+        elementTextBoxValue = input;
     }
 
 }
