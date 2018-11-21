cf2e88082533a85bd5ff2659295ba41a9e54e8f6
DESKTOP-A6SID3J\gun95 <gun958833@gmail.com>
2018-11-21 22:59:16 +0300

    init View

---
+++ code/frac-calc/View/src/main/java/ru/unn/agile/fraction/view/Fraction.java
@@ -0,0 +1,14 @@
+package ru.unn.agile.fraction.view;
+
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class Fraction {
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
