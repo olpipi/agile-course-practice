3e13622f8b36c482d572dde47cb1b61a7f69c01f
Aleksandr Fytov <alexandr.fytov@gmail.com>
2018-11-22 02:59:07 +0300

    Created string properties for all fields & initialValue test

--- code/roman-number-converter/ViewModel/src/main/java/ru/unn/agile/romannumberconverter/viewmodel/ViewModel.java
+++ code/roman-number-converter/ViewModel/src/main/java/ru/unn/agile/romannumberconverter/viewmodel/ViewModel.java
@@ -1,4 +1,26 @@
 package ru.unn.agile.romannumberconverter.viewmodel;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class ViewModel {
+    private StringProperty arabicValueStr = new SimpleStringProperty();
+    private StringProperty romanValueStr = new SimpleStringProperty();
+
+    public String getArabicValueStr() {
+        return arabicValueStr.get();
+    }
+
+    public String getRomanValueStr() {
+        return romanValueStr.get();
+    }
+
+    public ViewModel() {
+        initialize();
+    }
+
+    private void initialize() {
+        arabicValueStr.set("");
+        romanValueStr.set("");
+    }
 }
--- code/roman-number-converter/ViewModel/src/test/java/ru/unn/agile/romannumberconverter/viewmodel/ViewModelTest.java
+++ code/roman-number-converter/ViewModel/src/test/java/ru/unn/agile/romannumberconverter/viewmodel/ViewModelTest.java
@@ -1,4 +1,15 @@
 package ru.unn.agile.romannumberconverter.viewmodel;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ViewModelTest {
+    @Test
+    public void initViewModelWithDefaultValues() {
+        ViewModel viewModel = new ViewModel();
+
+        assertEquals("", viewModel.getArabicValueStr());
+        assertEquals("", viewModel.getRomanValueStr());
+    }
 }
