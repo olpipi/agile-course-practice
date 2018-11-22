be497f46001ca2d8bb2cef9b375e3be4b4660313
Osechkin Sergey <osechkin.s.v@gmail.com>
2018-11-22 13:58:42 +0300

    go Travis

--- code/salary-calculator/ViewModel/src/main/java/ru/unn/agile/salarycalculator/viewmodel/legacy/ViewModel.java
+++ code/salary-calculator/ViewModel/src/main/java/ru/unn/agile/salarycalculator/viewmodel/legacy/ViewModel.java
@@ -39,7 +39,7 @@ public class ViewModel {
                 "Worked houses must be between 1 and 500";
         public static final String CASH = "This your cash";
 
-        private Status(){
+        private Status() {
 
         }
     }
