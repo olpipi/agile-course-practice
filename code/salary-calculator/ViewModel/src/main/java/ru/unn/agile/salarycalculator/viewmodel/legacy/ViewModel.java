8fb7b19b93a841aa7590d1a15f37470bac0701da
Osechkin Sergey <osechkin.s.v@gmail.com>
2018-11-22 12:57:30 +0300

    edit class

--- code/salary-calculator/ViewModel/src/main/java/ru/unn/agile/salarycalculator/viewmodel/legacy/ViewModel.java
+++ code/salary-calculator/ViewModel/src/main/java/ru/unn/agile/salarycalculator/viewmodel/legacy/ViewModel.java
@@ -38,10 +38,6 @@ public class ViewModel {
         public static final String BAD_WORKED_HOURS_FORMAT =
                 "Worked houses must be between 1 and 500";
         public static final String CASH = "This your cash";
-
-        public Status(){
-            
-        }
     }
 
     public void checkCountFields() {
