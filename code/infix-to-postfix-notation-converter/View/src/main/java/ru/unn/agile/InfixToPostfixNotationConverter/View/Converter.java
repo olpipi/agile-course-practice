d7c45332a55b03639637edb71a70fc4fd6b0ac03
Moiseev Nikita <nikita.moiseev@intel.com>
2018-11-24 15:54:11 +0300

    Initial View commit: add .fxml file and Converter.java skeleton

---
+++ code/infix-to-postfix-notation-converter/View/src/main/java/ru/unn/agile/InfixToPostfixNotationConverter/View/Converter.java
@@ -0,0 +1,26 @@
+package ru.unn.agile.InfixToPostfixNotationConverter.View;
+
+//import javafx.event.ActionEvent;
+//import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.TextField;
+//import javafx.scene.text.Text;
+
+public class Converter {
+    //@FXML
+    //private TextField infixExpression;
+    //@FXML
+    //private TextField postfixExpression;
+    //@FXML
+    //private TextField expressionResult;
+    //@FXML
+    //private Text status;
+    //@FXML
+    //private Button convertAndCalculateButton;
+
+    @FXML
+    void initialize() {
+        //empty for now
+    }
+}
