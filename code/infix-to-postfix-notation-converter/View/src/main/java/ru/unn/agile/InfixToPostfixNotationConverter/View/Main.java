b86d700908fee776015c635f234e3d9474f30f5a
Moiseev Nikita <nikita.moiseev@intel.com>
2018-11-24 17:15:08 +0300

    Use FXMLLoader in Main.java

--- code/infix-to-postfix-notation-converter/View/src/main/java/ru/unn/agile/InfixToPostfixNotationConverter/View/Main.java
+++ code/infix-to-postfix-notation-converter/View/src/main/java/ru/unn/agile/InfixToPostfixNotationConverter/View/Main.java
@@ -9,9 +9,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(final Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(
-                Thread.currentThread().getContextClassLoader().getResource("Converter.fxml")
-        );
+        Parent root = FXMLLoader.load(getClass().getResource("Converter.fxml"));
         primaryStage.setTitle("Converter");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
