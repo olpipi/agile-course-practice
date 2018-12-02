1cd0f4af74cd0c8f216d5483f2746e222f8c8dab
Moskalenko Viktor <moskalenkoviktor@list.ru>
2018-12-02 20:08:31 +0300

    added face view test

--- code/shape-volume-calculator/view/src/main/java/ru/unn/agile/shapevolume/view/VolumeCalculator.java
+++ code/shape-volume-calculator/view/src/main/java/ru/unn/agile/shapevolume/view/VolumeCalculator.java
@@ -1,12 +1,3 @@
 package ru.unn.agile.shapevolume.view;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
-
-public class VolumeCalculator {
-
-}
+public class VolumeCalculator { }
