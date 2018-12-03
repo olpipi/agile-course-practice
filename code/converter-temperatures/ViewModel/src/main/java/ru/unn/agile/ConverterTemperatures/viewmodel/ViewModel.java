b6df0dc1acc55067cbd80983ec9b308db0a9512a
Ekaterina Shiryaeva <shiryaevae01@gmail.com>
2018-12-03 21:34:26 +0300

    Added string properties to ViewModel

--- code/converter-temperatures/ViewModel/src/main/java/ru/unn/agile/ConverterTemperatures/viewmodel/ViewModel.java
+++ code/converter-temperatures/ViewModel/src/main/java/ru/unn/agile/ConverterTemperatures/viewmodel/ViewModel.java
@@ -1,4 +1,59 @@
 package ru.unn.agile.ConverterTemperatures.viewmodel;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+//import javafx.collections.FXCollections;
+//import javafx.collections.ObservableList;
+import ru.unn.agile.ConverterTemperatures.model.TemperaturesUnit;
+
 public class ViewModel {
+    private final StringProperty convertFrom = new SimpleStringProperty();
+    private final StringProperty convertTo = new SimpleStringProperty();
+
+    //private final ObjectProperty<ObservableList<TemperaturesUnit>> scales =
+    //    new SimpleObjectProperty<>(FXCollections.observableArrayList(TemperaturesUnit.values()));
+    private final ObjectProperty<TemperaturesUnit> scale =
+        new SimpleObjectProperty<TemperaturesUnit>();
+
+    private final StringProperty status = new SimpleStringProperty();
+
+    public ViewModel() {
+        convertFrom.set("");
+        convertTo.set("");
+        scale.set(TemperaturesUnit.FAHRENHEIT);
+        status.set(Status.READY.toString());
+    }
+
+    public String getConvertFrom() {
+        return convertFrom.get();
+    }
+
+    public TemperaturesUnit getScale() {
+        return scale.get();
+    }
+
+    public String getConvertTo() {
+        return convertTo.get();
+    }
+
+    public final String getStatus() {
+        return status.get();
+    }
+}
+
+enum Status {
+    WAITING("Please provide input data"),
+    READY("Press 'Convert' or Enter"),
+    BAD_FORMAT("Bad format"),
+    SUCCESS("Success");
+
+    private final String name;
+    Status(final String name) {
+        this.name = name;
+    }
+    public String toString() {
+        return name;
+    }
 }
--- code/converter-temperatures/ViewModel/src/test/java/ru/unn/agile/ConverterTemperatures/viewmodel/ViewModelTest.java
+++ code/converter-temperatures/ViewModel/src/test/java/ru/unn/agile/ConverterTemperatures/viewmodel/ViewModelTest.java
@@ -1,4 +1,17 @@
 package ru.unn.agile.ConverterTemperatures.viewmodel;
 
+import org.junit.Test;
+import ru.unn.agile.ConverterTemperatures.model.TemperaturesUnit;
+
+import static org.junit.Assert.assertEquals;
+
 public class ViewModelTest {
+    @Test
+    public void createEmptyViewModel() {
+        ViewModel viewModel = new ViewModel();
+        assertEquals("", viewModel.getConvertFrom());
+        assertEquals(TemperaturesUnit.FAHRENHEIT, viewModel.getScale());
+        assertEquals("", viewModel.getConvertTo());
+        assertEquals(Status.READY.toString(), viewModel.getStatus());
+    }
 }
