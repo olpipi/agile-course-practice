368e37618abe597ff5d0445fc8b8560646e715bc
Ekaterina Shiryaeva <shiryaevae01@gmail.com>
2018-12-15 19:14:42 +0300

    Added input update and bad format processing

--- code/converter-temperatures/ViewModel/src/main/java/ru/unn/agile/ConverterTemperatures/viewmodel/ViewModel.java
+++ code/converter-temperatures/ViewModel/src/main/java/ru/unn/agile/ConverterTemperatures/viewmodel/ViewModel.java
@@ -4,16 +4,12 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-//import javafx.collections.FXCollections;
-//import javafx.collections.ObservableList;
 import ru.unn.agile.ConverterTemperatures.model.TemperaturesUnit;
 
 public class ViewModel {
     private final StringProperty convertFrom = new SimpleStringProperty();
     private final StringProperty convertTo = new SimpleStringProperty();
 
-    //private final ObjectProperty<ObservableList<TemperaturesUnit>> scales =
-    //    new SimpleObjectProperty<>(FXCollections.observableArrayList(TemperaturesUnit.values()));
     private final ObjectProperty<TemperaturesUnit> scale =
         new SimpleObjectProperty<TemperaturesUnit>();
 
@@ -26,20 +22,37 @@ public class ViewModel {
         status.set(Status.READY.toString());
     }
 
-    public String getConvertFrom() {
-        return convertFrom.get();
+    public StringProperty convertFromProperty() {
+        return convertFrom;
+    }
+    public StringProperty convertToProperty() {
+        return convertTo;
     }
 
     public TemperaturesUnit getScale() {
         return scale.get();
     }
 
-    public String getConvertTo() {
-        return convertTo.get();
+    public StringProperty statusProperty() {
+        return status;
     }
-
     public final String getStatus() {
         return status.get();
+    }
+
+    public void updateInputValues() {
+        if (convertFrom.get().isEmpty()) {
+            status.set(Status.WAITING.toString());
+        }
+        try {
+            if (!convertFrom.get().isEmpty()) {
+                Double.parseDouble(convertFrom.get());
+            }
+            status.set(Status.READY.toString());
+        } catch (NumberFormatException nfe) {
+            status.set(Status.BAD_FORMAT.toString());
+        }
+
     }
 }
 
--- code/converter-temperatures/ViewModel/src/test/java/ru/unn/agile/ConverterTemperatures/viewmodel/ViewModelTest.java
+++ code/converter-temperatures/ViewModel/src/test/java/ru/unn/agile/ConverterTemperatures/viewmodel/ViewModelTest.java
@@ -1,17 +1,48 @@
 package ru.unn.agile.ConverterTemperatures.viewmodel;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import ru.unn.agile.ConverterTemperatures.model.TemperaturesUnit;
 
 import static org.junit.Assert.assertEquals;
 
 public class ViewModelTest {
+    private ViewModel viewModel;
+
+    @Before
+    public void initViewModel() {
+        viewModel = new ViewModel();
+    }
+
+    @After
+    public void nullViewModel() {
+        viewModel = null;
+    }
+
     @Test
     public void createEmptyViewModel() {
-        ViewModel viewModel = new ViewModel();
-        assertEquals("", viewModel.getConvertFrom());
+        assertEquals("", viewModel.convertFromProperty().get());
         assertEquals(TemperaturesUnit.FAHRENHEIT, viewModel.getScale());
-        assertEquals("", viewModel.getConvertTo());
+        assertEquals("", viewModel.convertToProperty().get());
         assertEquals(Status.READY.toString(), viewModel.getStatus());
     }
+
+    @Test
+    public void canAddValueToConvert() {
+        viewModel.convertFromProperty().set("10.0");
+        viewModel.updateInputValues();
+
+        assertEquals("10.0", viewModel.convertFromProperty().get());
+        assertEquals(Status.READY.toString(), viewModel.getStatus());
+    }
+
+    @Test
+    public void canProcessBadInputFormat() {
+        viewModel.convertFromProperty().set("a");
+        viewModel.updateInputValues();
+
+        assertEquals(Status.BAD_FORMAT.toString(), viewModel.getStatus());
+    }
+
 }
