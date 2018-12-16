b673bcf7aeea515717fec424ebaddd3e8c8546d8
Maria-Oleneva <olenevamaria@yande.ru>
2018-12-16 23:40:51 +0300

    Txt Logger

--- code/quadratic-equation-solver/View/src/main/java/ru/unn/agile/quadraticequation/view/QuadraticEquation.java
+++ code/quadratic-equation-solver/View/src/main/java/ru/unn/agile/quadraticequation/view/QuadraticEquation.java
@@ -5,8 +5,8 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import ru.unn.agile.quadraticequation.viewmodel.ViewModel;
 import ru.unn.agile.quadraticequation.infrastructure.TxtLogger;
+import ru.unn.agile.quadraticequation.viewmodel.ViewModel;
 
 public class QuadraticEquation {
     @FXML
